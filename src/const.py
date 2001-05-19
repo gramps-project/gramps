84c2269e1c66b5e3675935cf87340b93ad5ef2a6
Don Allingham <don@gramps-project.org>
2001-05-19 02:59:19 +0000

    Moved to 0.1.4
    svn: r32

--- src/const.py
+++ src/const.py
@@ -67,7 +67,7 @@ gtkrcFile      = rootDir + os.sep + "gtkrc"
 #
 #-------------------------------------------------------------------------
 progName     = "gramps"
-version      = "0.1.4pre"
+version      = "0.1.4"
 copyright    = "(C) 2001 Donald N. Allingham"
 authors      = ["Donald N. Allingham"]
 comments     = _("Gramps (Genealogical Research and Analysis Management ") +\
