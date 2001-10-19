789425b14f6c7ac47a19b0854d8363f7bd3db294
Don Allingham <don@gramps-project.org>
2001-10-19 00:13:23 +0000

    Profiling code
    svn: r486

--- src/ReadXML.py
+++ src/ReadXML.py
@@ -232,16 +232,12 @@ def loadRevision(database, file, filename, revision, callback=None):
 
 if __name__ == "__main__":
     import profile
-    import cPickle
     
     database = RelDataBase()
     t1 = time.time()
-    #profile.run('loadData(database, sys.argv[1])')
-    loadData(database,sys.argv[1])
+    if len(sys.argv) > 2:
+        profile.run('loadData(database, sys.argv[1])')
+    else:
+        loadData(database,sys.argv[1])
     t2 = time.time()
     print t2-t1
-    f = gzip.open("autosave","w")
-    p = cPickle.dump(database,f)
-    f.close()
-    t3 = time.time()
-    print t3-t2
