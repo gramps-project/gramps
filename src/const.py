bcc4f65f613f4ade9978716c3061017e7e0c85ab
Don Allingham <don@gramps-project.org>
2002-01-31 05:21:31 +0000

    Collect more information at startup
    svn: r738

--- src/const.py
+++ src/const.py
@@ -85,7 +85,7 @@ gtkrcFile      = "%s/gtkrc" % rootDir
 #
 #-------------------------------------------------------------------------
 progName     = "gramps"
-version      = "0.7.1"
+version      = "0.7.2pre"
 copyright    = "© 2001 Donald N. Allingham"
 authors      = ["Donald N. Allingham", "David Hampton"]
 comments     = _("Gramps (Genealogical Research and Analysis Management Programming System) is a personal genealogy program.")
