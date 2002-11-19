862d75e46f1abe6a90d3d9bd32b5bd03d90dba41
Bruce DeGrasse <bdegrasse1@users.sourceforge.net>
2002-11-19 22:53:02 +0000

    Update Date and Place Text
    svn: r1198

--- src/plugins/DetAncestralReport.py
+++ src/plugins/DetAncestralReport.py
@@ -1,7 +1,7 @@
 #
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2000 Bruce J. DeGrasse
+# Copyright (C) 2002 Bruce J. DeGrasse
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
