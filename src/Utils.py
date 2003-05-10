ebec774ea0f861811e64c3e906fc16e48e9c738e
Alex Roitman <shura@gramps-project.org>
2003-05-10 06:17:07 +0000

    Roman numbers
    svn: r1510

--- src/Utils.py
+++ src/Utils.py
@@ -521,3 +521,28 @@ def set_titles(window,title,t,msg=None):
         window.set_title('%s - GRAMPS' % msg)
     else:
         window.set_title('%s - GRAMPS' % t)
+
+#-------------------------------------------------------------------------
+#
+#  Roman numbers
+#
+#-------------------------------------------------------------------------
+def roman(num):
+    "Simple-and-dirty way to get roman numerals for small numbers."
+
+    romans = {
+        1 : 'I', 2 : 'II', 3 : 'III', 4 : 'IV', 5 : 'V', 
+        6 : 'VI', 7 : 'VII', 8 : 'VIII', 9 : 'IX', 10 : 'X', 
+        11 : 'XI', 12 : 'XII', 13 : 'XIII', 14 : 'XIV', 15 : 'XV', 
+        16 : 'XVI', 17 : 'XVII', 18 : 'XVIII', 19 : 'XIX', 20 : 'XX', 
+        21 : 'XXI', 22 : 'XXII', 23 : 'XXIII', 24 : 'XXIV', 25 : 'XXV', 
+        26 : 'XXVI', 27 : 'XXVII', 28 : 'XXVIII', 29 : 'XXIX', 30 : 'XXX', 
+        31 : 'XXXI', 32 : 'XXXII', 33 : 'XXXIII', 34 : 'XXXIV', 35 : 'XXXV', 
+        36 : 'XXXVI', 37 : 'XXXVII', 38 : 'XXXVIII', 39 : 'XXXIX', 40 : 'XV' 
+    }
+    
+    if isinstance(num,type(1)) and num<=41:
+        rnum = romans[num]
+        return rnum
+    else:
+        return
