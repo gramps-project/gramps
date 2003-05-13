28094e6aa96ca52648c09b8baa0e9bc3a837d990
Alex Roitman <shura@gramps-project.org>
2003-05-13 15:29:22 +0000

    Typo
    svn: r1521

--- src/ReadXML.py
+++ src/ReadXML.py
@@ -1,4 +1,4 @@
-
+#
 # Gramps - a GTK+/GNOME based genealogy program
 #
 # Copyright (C) 2000  Donald N. Allingham
@@ -192,7 +192,7 @@ def importData(database, filename, callback):
         except:
             # File is lost => ask what to do
             MissingMediaDialog(_("Media object could not be found"),
-	        _("%(file_name)s was references in the database, but no longer exists. " 
+	        _("%(file_name)s is referenced in the database, but no longer exists. " 
                     "The file may have been deleted or moved to a different location. " 
                     "You may choose to either remove the reference from the database, " 
                     "keep the reference to the missing file, or select a new file." 
