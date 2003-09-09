4ba080738c2be14a2b7176850eb76bb9115a952f
Don Allingham <don@gramps-project.org>
2003-09-09 01:48:50 +0000

    update
    svn: r2109

--- gramps2/src/docgen/PdfDoc.py
+++ gramps2/src/docgen/PdfDoc.py
@@ -292,7 +292,6 @@ class PdfDoc(BaseDoc.BaseDoc):
             act_width = x_cm/ratio
 
         self.story.append(Spacer(1,0.5*cm))
-        print Image
         self.story.append(Image(name,act_width*cm,act_height*cm))
         self.story.append(Spacer(1,0.5*cm))
         self.image = 1
