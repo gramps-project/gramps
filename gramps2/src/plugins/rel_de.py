f9b0f6c78c47cf2008321e92c577c2e03b159a68
Alex Roitman <shura@gramps-project.org>
2004-01-09 02:29:57 +0000

    Update
    svn: r2612

--- gramps2/src/plugins/rel_de.py
+++ gramps2/src/plugins/rel_de.py
@@ -19,6 +19,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
+# $Id$
+
 #
 # Written by Alex Roitman, largely based on Relationship.py by Don Allingham.
 # and on valuable input from Dr. Martin Senftleben
