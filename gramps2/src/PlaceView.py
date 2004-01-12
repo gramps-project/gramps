f021ade7b2df0652fa496f812b154a66a8571894
Alex Roitman <shura@gramps-project.org>
2004-01-12 18:56:24 +0000

    Update
    svn: r2631

--- gramps2/src/PlaceView.py
+++ gramps2/src/PlaceView.py
@@ -1,7 +1,7 @@
 #
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2001-2003  Donald N. Allingham
+# Copyright (C) 2001-2004  Donald N. Allingham
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
