78fc879948371ece9a48bfd535b00c7b24355bed
Alex Roitman <shura@gramps-project.org>
2004-02-05 19:37:58 +0000

    * src/plugins/Merge.py (gen_key,name_compare): Fall back to not using soundex if the charset can't be latin-1 encoded.
    svn: r2778

--- gramps2/src/plugins/Merge.py
+++ gramps2/src/plugins/Merge.py
@@ -1,7 +1,7 @@
 #
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2000  Donald N. Allingham
+# Copyright (C) 2000-2004  Donald N. Allingham
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,6 +17,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+
+# $Id$
 
 "Database Processing/Merge people"
 
@@ -292,13 +294,19 @@ class Merge:
         
     def gen_key(self,val):
         if self.use_soundex:
-            return soundex.soundex(val)
+            try:
+                return soundex.soundex(val)
+            except UnicodeEncodeError:
+                return val
         else:
             return val
 
     def name_compare(self,s1,s2):
         if self.use_soundex:
-            return soundex.compare(s1,s2)
+            try:
+                return soundex.compare(s1,s2)
+            except UnicodeEncodeError:
+                return s1 == s2
         else:
             return s1 == s2
 
