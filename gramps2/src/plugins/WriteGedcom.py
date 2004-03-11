04bb7a785eb7bf4b4b3543428f91ab73775245b5
Alex Roitman <shura@gramps-project.org>
2004-03-11 23:14:55 +0000

    * src/plugins/WriteFtree.py: Typo. * src/plugins/WriteGedcom.py: Typo.
    svn: r3005

--- gramps2/src/plugins/WriteGedcom.py
+++ gramps2/src/plugins/WriteGedcom.py
@@ -705,12 +705,12 @@ class GedcomWriter:
             person = family.get_father_id()
             if person != None and self.plist.has_key(person.get_id()):
                 self.writeln("1 HUSB @%s@" % self.pid(person.get_id()))
-                father_alive = person.probably_alive(db)
+                father_alive = person.probably_alive(self.db)
 
             person = family.get_mother_id()
             if person != None and self.plist.has_key(person.get_id()):
                 self.writeln("1 WIFE @%s@" % self.pid(person.get_id()))
-                mother_alive = person.probably_alive(db)
+                mother_alive = person.probably_alive(self.db)
 
             if not self.restrict or ( not father_alive and not mother_alive ):
                 self.write_ord("SLGS",family.get_lds_sealing(),1,const.lds_ssealing)
@@ -788,7 +788,7 @@ class GedcomWriter:
 
     def write_person(self,person):
         self.writeln("0 @%s@ INDI" % self.pid(person.get_id()))
-        restricted = self.restrict and person.probably_alive (db)
+        restricted = self.restrict and person.probably_alive (self.db)
         self.prefn(person)
         primaryname = person.get_primary_name ()
         if restricted and self.living:
