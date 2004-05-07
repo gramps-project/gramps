253eb3ebfefb4ec1dc03075b53573d0271fc937a
Alex Roitman <shura@gramps-project.org>
2004-05-07 04:27:16 +0000

    Minor updates
    svn: r3138

--- gramps2/src/SelectPerson.py
+++ gramps2/src/SelectPerson.py
@@ -1,7 +1,7 @@
 #
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2003  Donald N. Allingham
+# Copyright (C) 2003-2004  Donald N. Allingham
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,6 +17,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+
+# $Id$
 
 #-------------------------------------------------------------------------
 #
