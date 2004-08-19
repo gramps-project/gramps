ec9ab8a6d9b1a38f6f6825844c5bbbb2677f9b86
Alex Roitman <shura@gramps-project.org>
2004-08-19 03:33:11 +0000

    * configure.in: Check for pygtk instead of pygtk-dev. * src/accent.py: Replace unknown chars with "?".
    svn: r3434

--- src/accent.py
+++ src/accent.py
@@ -18,11 +18,11 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
+# $Id$
+
 def deaccent(s):
     if not isinstance(s,unicode): return s
-    return ''.join([deaccentdata.get(c,'~') for c in s])
-
-# TODO: Add a few math symbols to the bottom.
+    return ''.join([deaccentdata.get(c,'?') for c in s])
 
 deaccentdata = {
     # 7-bit ASCII non-printable
