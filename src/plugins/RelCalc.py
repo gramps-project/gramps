7a22617b521cf3078bc7bc50c86fbabe50d8f869
Tim Waugh <twaugh@redhat.com>
2004-08-20 11:58:06 +0000

    2004-08-20  Tim Waugh  <twaugh@redhat.com>         * src/plugins/RelCalc.py (__init__): Handle unknown birth dates.
    svn: r3449

--- src/plugins/RelCalc.py
+++ src/plugins/RelCalc.py
@@ -99,7 +99,10 @@ class RelCalc:
                 continue
             val = self.db.get_person_from_handle(key).get_display_info()
             event = self.db.find_event_from_handle(val[3])
-            self.clist.add([val[0],val[1],event.get_date(),val[5],val[6]],p.get_handle())
+            event_date = None
+            if event:
+                event_date = event.get_date ()
+            self.clist.add([val[0],val[1],event_date,val[5],val[6]],p.get_handle())
 
         self.clist.connect_model()
             
