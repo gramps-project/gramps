4e43d89fe32630592ac742ee6afdc7cfd0fe166b
Don Allingham <don@gramps-project.org>
2004-09-19 04:11:34 +0000

    * src/GrampsInMemDB.py: new base class for in memory databases, such as XML or GEDCOM * src/GrampGEDDB.py: use new GrampsInMemDB base class * src/GrampXMLDB.py: use new GrampsInMemDB base class * src/Makefile.am: add GrampsInMemDB.py * src/ReadGedcom.py: remove debug statements
    svn: r3555

--- gramps2/src/GrampsXMLDB.py
+++ gramps2/src/GrampsXMLDB.py
@@ -21,45 +21,26 @@
 # $Id$
 
 from RelLib import *
-from GrampsDbBase import *
+from GrampsInMemDB import *
 
 import ReadXML
 import WriteXML
-
-_UNDO_SIZE      = 1000
-
-#-------------------------------------------------------------------------
-#
-# ID regular expression
-#
-#-------------------------------------------------------------------------
-_id_reg = compile("%\d+d")
 
 #-------------------------------------------------------------------------
 #
 # GrampsXMLDB
 #
 #-------------------------------------------------------------------------
-class GrampsXMLDB(GrampsDbBase):
+class GrampsXMLDB(GrampsInMemDB):
     """GRAMPS database object. This object is a base class for other
     objects."""
 
     def __init__(self):
         """creates a new GrampsDB"""
-        GrampsDbBase.__init__(self)
+        GrampsInMemDB.__init__(self)
 
     def load(self,name,callback):
-        self.person_map = {}
-        self.family_map = {}
-        self.place_map  = {}
-        self.source_map = {}
-        self.media_map  = {}
-        self.event_map  = {}
-        self.metadata   = {}
-        self.filename = name
         self.id_trans = {}
-        self.eventnames = {}
-        self.undodb = []
 
         ReadXML.importData(self,name)
         
@@ -70,47 +51,4 @@ class GrampsXMLDB(GrampsDbBase):
 
     def close(self):
         WriteXML.quick_write(self,self.filename)
-
-    def get_surname_list(self):
-        a = {}
-        for person_id in self.get_person_handles(sort_handles=False):
-            p = self.get_person_from_handle(person_id)
-            a[p.get_primary_name().get_surname()] = 1
-        vals = a.keys()
-        vals.sort()
-        return vals
-
-    def get_person_event_type_list(self):
-        names = self.eventnames.keys()
-        a = {}
-        for name in names:
-            a[unicode(name)] = 1
-        vals = a.keys()
-        vals.sort()
-        return vals
-
-    def remove_person(self,handle,transaction):
-        self.genderStats.uncount_person (self.person_map[handle])
-        if transaction != None:
-            old_data = self.person_map.get(handle)
-            transaction.add(PERSON_KEY,handle,old_data)
-        del self.person_map[handle]
-
-    def remove_source(self,handle,transaction):
-        if transaction != None:
-            old_data = self.source_map.get(str(handle))
-            transaction.add(SOURCE_KEY,handle,old_data)
-        del self.source_map[str(handle)]
-
-    def remove_family_handle(self,handle,transaction):
-        if transaction != None:
-            old_data = self.family_map.get(str(handle))
-            transaction.add(FAMILY_KEY,handle,old_data)
-        del self.family_map[str(handle)]
-
-    def remove_event(self,handle,transaction):
-        if transaction != None:
-            old_data = self.event_map.get(str(handle))
-            transaction.add(EVENT_KEY,handle,old_data)
-        del self.event_map[str(handle)]
 
