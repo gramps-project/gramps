f0fbb72dc4fa01d8b1b57c315adfc0d92c4c902e
Don Allingham <don@gramps-project.org>
2004-09-21 03:17:07 +0000

    * src/GrampsBSDDB.py: fix remove_person and count_person * src/GrampsInMemDB.py: fix remove_person and count_person
    svn: r3566

--- src/GrampsInMemDB.py
+++ src/GrampsInMemDB.py
@@ -79,7 +79,8 @@ class GrampsInMemDB(GrampsDbBase):
         return vals
 
     def remove_person(self,handle,transaction):
-        self.genderStats.uncount_person (self.person_map[handle])
+        person = self.get_person_from_handle(handle)
+        self.genderStats.uncount_person (person)
         if transaction != None:
             old_data = self.person_map.get(handle)
             transaction.add(PERSON_KEY,handle,old_data)
