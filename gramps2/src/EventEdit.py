02a7643f8cade739764afe666c5423cb68b7076b
Alex Roitman <shura@gramps-project.org>
2004-10-07 12:38:31 +0000

    * src/EventEdit.py (update_event): Typo.
    svn: r3602

--- gramps2/src/EventEdit.py
+++ gramps2/src/EventEdit.py
@@ -349,7 +349,7 @@ class EventEditor:
         self.event.set_source_reference_list(self.srcreflist)
         self.event.set_witness_list(self.witnesslist)
         
-        if not dobj.are_equal(date):
+        if not dobj.is_equal(date):
             self.event.set_date_object(date)
             self.parent.lists_changed = 1
 
