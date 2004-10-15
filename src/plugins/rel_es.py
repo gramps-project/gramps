192a0a93bd9b9065e710fe0326f1b27fb2ed2e64
Alex Roitman <shura@gramps-project.org>
2004-10-15 01:34:22 +0000

    Update
    svn: r3632

--- src/plugins/rel_es.py
+++ src/plugins/rel_es.py
@@ -19,6 +19,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
+# Written by Julio Sanchez <julio.sanchez@gmail.com>
+
 # $Id$
 
 #-------------------------------------------------------------------------
