0c638f4595edcde474784e74805a08270acf7575
Alex Roitman <shura@gramps-project.org>
2005-01-11 16:56:51 +0000

    Update
    svn: r3900

--- src/GrampsIniKeys.py
+++ src/GrampsIniKeys.py
@@ -1,7 +1,7 @@
 #
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2004  Donald N. Allingham
+# Copyright (C) 2005  Donald N. Allingham
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-# $ $
+# $Id$
 
 """
 Low-level handling of .ini keys. 
