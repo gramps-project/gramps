0ea6138c33ad61c9c5bae59d3ba948f0c1b82525
Alex Roitman <shura@gramps-project.org>
2005-05-19 20:43:35 +0000

    * src/EditSource.py (button_press): Properly test event names.
    svn: r4635

--- gramps2/src/EditSource.py
+++ gramps2/src/EditSource.py
@@ -267,14 +267,19 @@ class EditSource:
         elif type == 2:
             import EventEdit
             event = self.db.get_event_from_handle(handle)
-            if event.get_name() in const.marriageEvents:
+            event_name = event.get_name()
+            if const.family_events.has_key(event_name):
                 EventEdit.EventEditor(
                     self,", ", const.marriageEvents, const.family_events,
                     event, None, 0, None, None, self.db.readonly)
-            elif event.get_name() in const.personalEvents + [_("Birth"),_("Death")]:
+            elif const.personal_events.has_key(event_name):
                 EventEdit.EventEditor(
                     self,", ", const.personalEvents, const.personal_events,
                     event, None, 0, None, None, self.db.readonly)
+            elif event_name in ["Birth","Death"]:
+                EventEdit.EventEditor(
+                    self,", ", const.personalEvents, const.personal_events,
+                    event, None, 1, None, None, self.db.readonly)
         elif type == 3:
             import EditPlace
             place = self.db.get_place_from_handle(handle)
