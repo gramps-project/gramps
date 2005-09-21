11e9730fb9978ce68bf1e0e1a9d5b5447b9d839e
Martin Hawlisch <martin.hawlisch@gmx.de>
2005-09-21 20:59:19 +0000

    * src/dates/Date_de.py: Fix month name
    svn: r5214

--- gramps2/src/dates/Date_de.py
+++ gramps2/src/dates/Date_de.py
@@ -59,7 +59,7 @@ class DateParserDE(DateParser):
     month_to_int[u"jenner"] = 1
     month_to_int[u"feber"]  = 2
     month_to_int[u"februaris"]  = 2
-    month_to_int[u"merz"]  = 2
+    month_to_int[u"merz"]  = 3
     month_to_int[u"aprilis"]  = 4
     month_to_int[u"maius"]  = 5
     month_to_int[u"junius"]  = 6
