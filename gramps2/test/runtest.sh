31e94e89bdd76d49c16af88d04cfb84b5ab1f595
Alex Roitman <shura@gramps-project.org>
2005-11-23 06:03:24 +0000

    * test/runtest.sh: Switch to doing many reports in one call.
    svn: r5423

--- gramps2/test/runtest.sh
+++ gramps2/test/runtest.sh
@@ -5,33 +5,40 @@ TDIR=$GDIR/test
 OPTS="-i $GDIR/example/gramps/example.gramps -f gramps-xml"
 PRG="python gramps.py"
 
-for report in ancestor_chart ancestor_chart2 descendant_graph
-do
-  for fmt in "sxw" "ps" "pdf" "svg"
-    do
-    echo ""
-    echo "+--------------------------------------------------------------"
-    echo "|"
-    echo "| $report in $fmt format"
-    echo "|"
-    echo "+--------------------------------------------------------------"
-    
-    (cd $GDIR/src; $PRG $OPTS -a report -p name=$report,id=I44,off=$fmt,of=$TDIR/$report.$fmt)
-  done
-done
+GRPH_FMT="sxw ps pdf svg"
+TEXT_FMT="sxw pdf kwd abw rtf"
 
-for report in ancestor_report ancestors_report descend_report det_ancestor_report det_descendant_report
-do
-  for fmt in "sxw" "pdf" "kwd" "abw" "rtf"
-    do
-    echo ""
-    echo "+--------------------------------------------------------------"
-    echo "|"
-    echo "| $report in $fmt format"
-    echo "|"
-    echo "+--------------------------------------------------------------"
-    
-    (cd $GDIR/src; $PRG $OPTS -a report -p name=$report,id=I44,off=$fmt,of=$TDIR/$report.$fmt)
-  done
-done
+GRPH_REP="ancestor_chart ancestor_chart2 descendant_graph"
+TEXT_REP="ancestor_report ancestors_report descend_report det_ancestor_report det_descendant_report"
 
+# Single run with all graphical reports in all formats
+action=
+for report in $GRPH_REP; do
+    for fmt in $GRPH_FMT; do
+	action="$action -a report -p name=$report,id=I44,off=$fmt,of=$TDIR/$report.$fmt"
+    done
+done
+echo ""
+echo "+--------------------------------------------------------------"
+echo "| Reports:"
+echo "|          "$GRPH_REP
+echo "| Formats:"
+echo "|          "$GRPH_FMT
+echo "+--------------------------------------------------------------"
+(cd $GDIR/src; $PRG $OPTS $action)
+
+# Single run with all textual reports in all formats
+action=
+for report in $TEXT_REP; do
+    for fmt in $TEXT_FMT; do
+	action="$action -a report -p name=$report,id=I44,off=$fmt,of=$TDIR/$report.$fmt"
+    done
+done
+echo ""
+echo "+--------------------------------------------------------------"
+echo "| Reports:"
+echo "|          "$TEXT_REP
+echo "| Formats:"
+echo "|          "$TEXT_FMT
+echo "+--------------------------------------------------------------"
+(cd $GDIR/src; $PRG $OPTS $action)
