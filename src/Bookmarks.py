8189db333eeae956f6d1d015be84e30174eabc6f
Don Allingham <don@gramps-project.org>
2005-12-26 05:11:37 +0000

    * src/Bookmarks.py: remove debugging statements * src/DisplayState.py: remove extra rebuild request * src/Plugins.py: start support of Report menu * src/ViewManager.py: tie in report support
    svn: r5634

--- src/Bookmarks.py
+++ src/Bookmarks.py
@@ -103,7 +103,6 @@ class Bookmarks :
         self.action_group.add_actions(actions)
         self.uimanager.insert_action_group(self.action_group,1)
         self.active = self.uimanager.add_ui_from_string(f.getvalue())
-        print f.getvalue()
         f.close()
 
     def add(self,person_handle):
