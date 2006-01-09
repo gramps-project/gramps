c5f2adb8587f41c87ebe076275392662ffba081f
Don Allingham <don@gramps-project.org>
2006-01-09 21:40:23 +0000

    * relation.svg: Relationship view icon * media.svg: Relationship view icon * src/ViewManager.py: no background color for sidebar * src/gramps_main.py: new icons
    svn: r5703

--- gramps2/src/gramps_main.py
+++ gramps2/src/gramps_main.py
@@ -66,9 +66,9 @@ def register_stock_icons ():
     import os
     items = [
         ('person.svg',('gramps-person','Person',gtk.gdk.CONTROL_MASK,0,'')),
-        ('family48.png',('gramps-family','Relationships',gtk.gdk.CONTROL_MASK,0,'')),
+        ('relation.svg',('gramps-family','Relationships',gtk.gdk.CONTROL_MASK,0,'')),
         ('family48.png',('gramps-family-list','Family List',gtk.gdk.CONTROL_MASK,0,'')),
-        ('media.png',('gramps-media','Media',gtk.gdk.CONTROL_MASK,0,'')),
+        ('media.svg',('gramps-media','Media',gtk.gdk.CONTROL_MASK,0,'')),
         ('ped24.png',('gramps-pedigree','Pedigree',gtk.gdk.CONTROL_MASK,0,'')),
         ('repos.png',('gramps-repository','Repositories',
                       gtk.gdk.CONTROL_MASK,0,'')),
