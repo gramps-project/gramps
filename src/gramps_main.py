7601f47a76d59155e3573615cad550f7807ec256
Don Allingham <don@gramps-project.org>
2006-01-09 03:30:22 +0000

    * src/person.svg: new icon * src/gramps_main.py: add person.svg
    svn: r5699

--- src/gramps_main.py
+++ src/gramps_main.py
@@ -66,7 +66,7 @@ iconpaths = [".",const.rootDir]
 def register_stock_icons ():
     import os
     items = [
-        ('people48.png',('gramps-person','Person',gtk.gdk.CONTROL_MASK,0,'')),
+        ('person.svg',('gramps-person','Person',gtk.gdk.CONTROL_MASK,0,'')),
         ('family48.png',('gramps-family','Relationships',gtk.gdk.CONTROL_MASK,0,'')),
         ('family48.png',('gramps-family-list','Family List',gtk.gdk.CONTROL_MASK,0,'')),
         ('media.png',('gramps-media','Media',gtk.gdk.CONTROL_MASK,0,'')),
