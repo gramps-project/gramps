f958ac04cfbe51e317ff9102ac81c08e68d2e351
Alex Roitman <shura@gramps-project.org>
2006-01-26 19:49:40 +0000

    'generic' for the namespace argument; Remove unneeded import.
    svn: r5837

--- gramps2/src/GenericFilter.py
+++ gramps2/src/GenericFilter.py
@@ -1,7 +1,7 @@
 #
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2002-2005  Donald N. Allingham
+# Copyright (C) 2002-2006  Donald N. Allingham
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -39,7 +39,6 @@ from xml.sax import make_parser,handler,SAXParseException
 import os
 import sets
 from gettext import gettext as _
-from TransUtils import strip_context as __
 
 #-------------------------------------------------------------------------
 #
@@ -58,7 +57,7 @@ import RelLib
 import Date
 import DateHandler
 import NameDisplay
-from TransTable import TransTable
+from TransUtils import strip_context as __
 from Utils import for_each_ancestor,probably_alive,get_source_referents
 
 #-------------------------------------------------------------------------
