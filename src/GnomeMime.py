2c47024571ecc37919607dad574d39fda7708dcb
Alex Roitman <shura@gramps-project.org>
2006-01-29 04:53:08 +0000

    * src/GnomeMime.py: Import neede modules.
    svn: r5852

--- src/GnomeMime.py
+++ src/GnomeMime.py
@@ -1,7 +1,7 @@
 #
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2000-2004  Donald N. Allingham
+# Copyright (C) 2000-2006  Donald N. Allingham
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,6 +17,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+
+# $Id$
+
+import gtk
+import const
 
 try:
     from gnomevfs import mime_get_short_list_applications, mime_get_description, get_mime_type
