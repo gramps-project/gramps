be30307de5bf04a3495acca216997eddf74cad97
Don Allingham <don@gramps-project.org>
2006-03-03 00:43:32 +0000

    rename modules
    svn: r6052

--- src/EditLocation.py
+++ src/EditLocation.py
@@ -18,7 +18,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-# $Id$
+# $Id: LocEdit.py 6051 2006-03-03 00:38:41Z dallingham $
 
 #-------------------------------------------------------------------------
 #
