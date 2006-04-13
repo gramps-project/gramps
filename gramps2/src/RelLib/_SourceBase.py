064e200d0df7bad34fe21fe6ac2690526e15c132
Alex Roitman <shura@gramps-project.org>
2006-04-13 05:28:31 +0000

    Update
    svn: r6325

--- gramps2/src/RelLib/_SourceBase.py
+++ gramps2/src/RelLib/_SourceBase.py
@@ -18,7 +18,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-# $Id: _SourceNote.py 5875 2006-02-03 22:03:53Z rshura $
+# $Id$
 
 """
 SourceBase class for GRAMPS
