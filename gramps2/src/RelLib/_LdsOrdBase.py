5b5cc6f6c74b2bb9af9375447ee4b4d3b9fb6253
Alex Roitman <shura@gramps-project.org>
2006-04-20 18:30:28 +0000

    Add keyword expansion
    svn: r6383

--- gramps2/src/RelLib/_LdsOrdBase.py
+++ gramps2/src/RelLib/_LdsOrdBase.py
@@ -18,7 +18,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-# $Id: _LdsOrdBase.py 5875 2006-02-03 22:03:53Z rshura $
+# $Id$
 
 """
 LdsOrdBase class for GRAMPS
