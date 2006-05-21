d642d361134bdd8c2eaae22f8a105a68e6983d75
Alex Roitman <shura@gramps-project.org>
2006-05-21 21:03:44 +0000

    2006-05-21  Alex Roitman  <shura@gramps-project.org> 	* src/plugins/Calendar.py (print_page): Use correct variable.
    svn: r6739

--- src/plugins/Calendar.py
+++ src/plugins/Calendar.py
@@ -202,7 +202,7 @@ class Calendar(Report.Report):
         self.doc.draw_bar("title", 0, 0, width, header)
         self.doc.draw_line("border", 0, header, width, header)
         year = self["year"]
-        title = "%s %d" % (GrampsLocale._months[month], year)
+        title = "%s %d" % (GrampsLocale.long_months[month], year)
         font_height = pt2cm(1.25 * self["title"].get_size())
         self.doc.center_text("title", title, width/2, font_height + self["offset"]) # 1.0
         cell_width = width / 7
