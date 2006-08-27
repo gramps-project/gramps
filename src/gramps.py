912a73371f139b88e7763dd166112eef217efda8
Alex Roitman <shura@gramps-project.org>
2006-08-27 21:04:49 +0000

    * src/GrampsDb/_WriteGedcom.py (GedcomWriter.gui_setup): Add a 	dialog about the researcher information. 	* src/gramps.py (run): Remove startup dialog.
    svn: r7278

--- src/gramps.py
+++ src/gramps.py
@@ -1,7 +1,7 @@
 #
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2000-2003  Donald N. Allingham
+# Copyright (C) 2000-2006  Donald N. Allingham
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,6 +17,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+
+# $Id$
 
 #-------------------------------------------------------------------------
 #
@@ -168,12 +170,7 @@ def run():
         pass
     
     try:        
-        import StartupDialog
-        
-        if StartupDialog.need_to_run():
-            StartupDialog.StartupDialog(gramps_main.Gramps,args)
-        else:
-            gramps_main.Gramps(args)
+        gramps_main.Gramps(args)
     except:
         log.error("Gramps failed to start.", exc_info=True)
 
