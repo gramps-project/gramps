12833ebaf576e273a8d4672cc659a9ac437d0dad
Zsolt Foldvari <zfoldvar@users.sourceforge.net>
2006-11-30 21:45:20 +0000

    2006-11-30  Zsolt Foldvari  <zfoldvar@users.sourceforge.net> 	* src/GrampsDb/_GrampsBSDDB.py (gramps_upgrade_9): Correctly convert 	alt_loc in Place upgrade.
    svn: r7738

--- gramps2/src/GrampsDb/_GrampsBSDDB.py
+++ gramps2/src/GrampsDb/_GrampsBSDDB.py
@@ -1875,7 +1875,7 @@ class GrampsBSDDB(GrampsDbBase,UpdateCallback):
             place = Place()
             place.handle = handle
             (junk_handle, place.gramps_id, place.title, place.long, place.lat,
-             place.main_loc, alt_loc, place.urls, place.media_list,
+             place.main_loc, place.alt_loc, place.urls, place.media_list,
              place.source_list, place.note, place.change) = info
 
             # Cover attributes contained in MediaRefs
