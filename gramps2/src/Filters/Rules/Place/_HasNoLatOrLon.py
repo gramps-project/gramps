bdb5ea0dd89b3f610a49f76f9ee1ca661ecf4c61
Don Allingham <don@gramps-project.org>
2007-01-26 05:10:51 +0000

    svn: r7986

---
+++ gramps2/src/Filters/Rules/Place/_HasNoLatOrLon.py
@@ -0,0 +1,53 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2002-2006  Donald N. Allingham
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+
+#-------------------------------------------------------------------------
+#
+# Standard Python modules
+#
+#-------------------------------------------------------------------------
+from gettext import gettext as _
+
+#-------------------------------------------------------------------------
+#
+# GRAMPS modules
+#
+#-------------------------------------------------------------------------
+from Filters.Rules._Rule import Rule
+
+#-------------------------------------------------------------------------
+#
+# HasNoLatOrLon
+#
+#-------------------------------------------------------------------------
+class HasNoLatOrLon(Rule):
+    """Rule that checks if Latitude or Longitude are not given"""
+
+
+    labels      = []
+    name        = _('Places with no latitude or longitude given')
+    description = _("Matches places with latitude or longitude empty")
+    category    = _('Position filters')
+
+    def apply(self,db,place):
+        if place.get_latitude().strip and place.get_longitude().strip() :
+            return False
+        return True
