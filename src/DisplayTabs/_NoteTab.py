5ae0ac4550abd81bfd388bea7647cd0b100485aa
Alex Roitman <shura@gramps-project.org>
2007-02-26 07:02:29 +0000

    Update
    svn: r8243

--- src/DisplayTabs/_NoteTab.py
+++ src/DisplayTabs/_NoteTab.py
@@ -1,7 +1,7 @@
 #
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2000-2006  Donald N. Allingham
+# Copyright (C) 2000-2007  Donald N. Allingham
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -34,7 +34,7 @@ from gettext import gettext as _
 #-------------------------------------------------------------------------
 import Spell
 import Errors
-
+import RelLib
 from DisplayTabs import log
 from _NoteModel import NoteModel
 from _EmbeddedList import EmbeddedList
