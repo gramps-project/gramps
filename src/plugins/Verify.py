e53f5133da05dbe0f09e67cffd0f9aa2fdf58ccf
Alex Roitman <shura@gramps-project.org>
2007-06-17 23:32:42 +0000

    * src/plugins/Verify.py: Clarification in wording: #1071.
    svn: r8573

--- src/plugins/Verify.py
+++ src/plugins/Verify.py
@@ -1,7 +1,7 @@
 #
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2000-2006  Donald N. Allingham
+# Copyright (C) 2000-2007  Donald N. Allingham
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -1473,7 +1473,6 @@ register_tool(
     tool_class = Verify,
     options_class = VerifyOptions,
     modes = Tool.MODE_GUI | Tool.MODE_CLI,
-    translated_name = _("Verify the database"),
-    description = _("Lists exceptions to assertions or checks "
-                    "about the database")
+    translated_name = _("Verify the data"),
+    description = _("Verifies the data against user-defined tests")
     )
