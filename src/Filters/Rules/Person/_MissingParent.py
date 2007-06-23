d1f776c3bb09095b9fe5b66f7e2a7f8c1110156c
Alex Roitman <shura@gramps-project.org>
2007-06-23 21:51:57 +0000

    2007-06-23  Alex Roitman  <shura@gramps-project.org> 	* POTFILES.in: Add new file.
    2007-06-16  Peter Landgren  <peter.talken@telia.com>
    	* sv.po: Update.
    
    2007-06-16  Matthias Alles  <alles@rhrk.uni-kl.de>
    	* de.po: Fixes.
    
    2007-06-15  Matthias Alles  <alles@rhrk.uni-kl.de>
    	* de.po: Fix the "was the son of" problem.
    
    2007-06-13  Morten Bo Johansen  <mbj@spamcop.net>
    	* da.po: Translation update.
    
    2007-05-24  Lubo Vasko  <pgval@inMail.sk>
    	* sk.po: Translation update.
    
    2007-05-21 Arturas Sleinius  <asleinius@users.sourceforge.net>
            * lt.po: updated Lithuanian translation
    
    2007-05-21  Stefan Björk  <betula@users.sourceforge.net>
    	* sv.po: Translation update; bug #0001058.
    
    2007-05-17  Espen Berg  <espen.berg@ntnu.no>
    	* nb.po: Translation update
    	* no.po: Translation update
    
    2007-05-14  Espen Berg  <espen.berg@ntnu.no>
    	* nb.po: Translation update
    	* no.po: Translation update
    
    2007-04-17  Lubo Vasko  <pgval@inMail.sk>
    	* sk.po: Translation update.
    
    2007-04-17  Piotr Czubaszek  <pioterus@gmail.com>
    	* pl.po: Translation update.
    
    2007-04-17 Arturas Sleinius  <asleinius@users.sourceforge.net>
    	* lt.po: updated Lithuanian translation
    
    2007-03-08  Stefan Björk  <betula@users.sourceforge.net>
    	* sv.po: Translation update.
    
    2007-03-07  Stefan Björk  <betula@users.sourceforge.net>
    	* sv.po: Translation update.
    
    2007-02-20  Espen Berg  <espen.berg@ntnu.no>
    	* nb.po: Translation update
    	* no.po: Translation update
    
    2007-02-15  Piotr Czubaszek  <pioterus@gmail.com>
    	* pl.po: Translation update.
    
    2007-02-14  Espen Berg  <espen.berg@ntnu.no>
    	* nb.po: Translation update
    	* no.po: Translation update
    
    
    
    svn: r8640

--- src/Filters/Rules/Person/_MissingParent.py
+++ src/Filters/Rules/Person/_MissingParent.py
@@ -19,6 +19,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
+# $Id$
 
 #-------------------------------------------------------------------------
 #
@@ -59,4 +60,3 @@ class MissingParent(Rule):
             if not mother_handle:
                 return True
         return False
-        
