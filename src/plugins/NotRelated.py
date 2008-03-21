9f3ba337e107b9a5c4f2e5a17ebc9cbb14016511
Erik De Richter <erikdrgm@users.sourceforge.net>
2008-03-21 12:18:27 +0000

    /src/plugins/NotRelated.py
    svn: r10361

--- src/plugins/NotRelated.py
+++ src/plugins/NotRelated.py
@@ -28,7 +28,6 @@
 #
 #------------------------------------------------------------------------
 import os
-from gettext import gettext as _
 
 #------------------------------------------------------------------------
 #
@@ -49,7 +48,17 @@ from Editors import EditPerson, EditFamily
 from QuestionDialog import WarningDialog
 import ManagedWindow
 import Utils
+import GrampsDisplay
+from TransUtils import sgettext as _
 
+
+#-------------------------------------------------------------------------
+#
+# Constants
+#
+#-------------------------------------------------------------------------
+WIKI_HELP_PAGE = 'Gramps_3.0_Wiki_Manual_-_Reports'
+WIKI_HELP_SEC = _('manual|Not_Related...')
 #------------------------------------------------------------------------
 #
 # 
@@ -67,7 +76,10 @@ class NotRelated(ManagedWindow.ManagedWindow) :
         self.db = dbstate.db
         glade_file = "%s/NotRelated.glade" % os.path.dirname(__file__)
         topDialog = gtk.glade.XML(glade_file, "top", "gramps")
-        topDialog.signal_autoconnect({"destroy_passed_object" : self.close})
+        topDialog.signal_autoconnect({
+            "destroy_passed_object" : self.close,
+            "on_help_clicked"       : self.on_help_clicked,
+        })
 
         window = topDialog.get_widget("top")
         title = topDialog.get_widget("title")
@@ -79,6 +91,7 @@ class NotRelated(ManagedWindow.ManagedWindow) :
         self.markerapply.set_sensitive(False)
         self.markerapply.connect('clicked', self.applyMarkerClicked)
 
+    
         # start the progress indicator
         self.progress = Utils.ProgressMeter(self.title,_('Starting'))
 
@@ -197,6 +210,10 @@ class NotRelated(ManagedWindow.ManagedWindow) :
                     EditPerson(self.dbstate, self.uistate, [], person)
                 except Errors.WindowActiveError:
                     pass
+#   
+    def on_help_clicked(self, obj):
+        """Display the relevant portion of GRAMPS manual"""
+        GrampsDisplay.help('tools-util-other', WIKI_HELP_PAGE , WIKI_HELP_SEC)    
 
 
     def applyMarkerClicked(self, button) :
