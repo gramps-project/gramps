66b789434ed5330ee82ccf7d6831cd1473c23366
Zsolt Foldvari <zfoldvar@users.sourceforge.net>
2008-04-05 22:37:58 +0000

    Add missing $Id$ line.
    svn: r10491

--- src/DbManager.py
+++ src/DbManager.py
@@ -18,6 +18,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
+# $Id$
+
 """
 Provide the management of databases. This includes opening, renaming,
 creating, and deleting of databases.
