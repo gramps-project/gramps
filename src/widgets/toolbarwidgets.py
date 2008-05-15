373fb6c82f434850ed55478f9ff71fde7ccfad39
Zsolt Foldvari <zfoldvar@users.sourceforge.net>
2008-05-15 10:58:18 +0000

    Using recommended absolut import for modules in the same package instead.
    svn: r10722

--- src/widgets/toolbarwidgets.py
+++ src/widgets/toolbarwidgets.py
@@ -43,7 +43,7 @@ import gtk
 # GRAMPS modules
 #
 #-------------------------------------------------------------------------
-from .multitypecomboentry import MultiTypeComboEntry
+from widgets.multitypecomboentry import MultiTypeComboEntry
 
 #-------------------------------------------------------------------------
 #
