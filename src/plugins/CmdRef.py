6bff7924897e501822c8a781757c9c137d844009
Brian Matherly <brian@gramps-project.org>
2008-09-17 12:04:13 +0000

    0002389: Command line reference tool is broken (plugins/CmdRef.py)
    svn: r11069

--- src/plugins/CmdRef.py
+++ src/plugins/CmdRef.py
@@ -177,19 +177,12 @@ class CmdRef(Tool.Tool):
 
             if len(oclass.options_help[arg])>2:
                 if type(oclass.options_help[arg][2]) in [list,tuple]:
-                    if oclass.options_help[arg][3]:
-                        f.write('          <orderedlist>\n')
+                    if oclass.options_help[arg][2]:
+                        f.write('          <itemizedlist>\n')
                         for val in oclass.options_help[arg][2]:
-                            f.write( "      <listitem><para>%s</para></listitem>\n"
-                                     % escape(val))
-                        f.write('          </orderedlist>\n')
-                    else:
-                        if oclass.options_help[arg][2]:
-                            f.write('          <itemizedlist>\n')
-                            for val in oclass.options_help[arg][2]:
-                                f.write( "      <listitem><para>%s</para>"
-                                         "</listitem>\n" % escape(val))
-                            f.write('          </itemizedlist>\n')
+                            f.write( "      <listitem><para>%s</para>"
+                                     "</listitem>\n" % escape(val))
+                        f.write('          </itemizedlist>\n')
                 else:
                     f.write('            '
                             '<para>Value: <userinput>%s</userinput></para>\n'
