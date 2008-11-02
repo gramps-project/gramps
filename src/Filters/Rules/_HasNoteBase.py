cb6228e4e38fe3868ba693ea1c37bac5743de4f8
Jérôme Rapinat <romjerome@yahoo.fr>
2008-11-02 09:36:45 +0000

    2465: More rules according to User Interface Object tabs
    svn: r11236

---
+++ src/Filters/Rules/_HasNoteBase.py
@@ -0,0 +1,49 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2002-2007  Donald N. Allingham
+# Copyright (C) 2007-2008  Brian G. Matherly
+# Copyright (C) 2008  Jerome Rapinat
+# Copyright (C) 2008  Benny Malengier
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+#-------------------------------------------------------------------------
+#
+# Standard Python modules
+#
+#-------------------------------------------------------------------------
+from gettext import gettext as _
+
+#-------------------------------------------------------------------------
+#
+# GRAMPS modules
+#
+#-------------------------------------------------------------------------
+from Filters.Rules._Rule import Rule
+
+#-------------------------------------------------------------------------
+# "Objects having notes"
+#-------------------------------------------------------------------------
+class HasNoteBase(Rule):
+    """Objects having notes"""
+
+    name        = _('Object with note')
+    description = _("Matches objects that have a note")
+    category    = _('General filters')
+
+    def apply(self, db, obj):
+        return len( obj.get_note_list()) > 0
