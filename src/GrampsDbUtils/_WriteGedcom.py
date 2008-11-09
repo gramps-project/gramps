ef5e5868432ec4faf833309fe589433b22d81c4a
Gary Burton <gary.burton@zen.co.uk>
2008-11-09 21:05:35 +0000

    GEDCOM specification fix. Bug 2491.
    svn: r11280

--- src/GrampsDbUtils/_WriteGedcom.py
+++ src/GrampsDbUtils/_WriteGedcom.py
@@ -2,6 +2,7 @@
 # Gramps - a GTK+/GNOME based genealogy program
 #
 # Copyright (C) 2000-2007  Donald N. Allingham
+# Copyright (C) 2008       Gary Burton
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -1296,15 +1297,15 @@ class GedcomWriter(BasicUtils.UpdateCallback):
         title = name.get_title()
         if suffix == "":
             if surprefix == "":
-                self.__writeln(1, 'NAME', '%s/%s/' % (firstname, surname))
+                self.__writeln(1, 'NAME', '%s /%s/' % (firstname, surname))
             else:
-                self.__writeln(1, 'NAME', '%s/%s %s/' % 
+                self.__writeln(1, 'NAME', '%s /%s %s/' % 
                                (firstname, surprefix, surname))
         elif surprefix == "":
-            self.__writeln(1, 'NAME', '%s/%s/ %s' % 
+            self.__writeln(1, 'NAME', '%s /%s/ %s' % 
                            (firstname, surname, suffix))
         else:
-            self.__writeln(1, 'NAME', '%s/%s %s/ %s' % 
+            self.__writeln(1, 'NAME', '%s /%s %s/ %s' % 
                            (firstname, surprefix, surname, suffix))
 
         if firstname:
