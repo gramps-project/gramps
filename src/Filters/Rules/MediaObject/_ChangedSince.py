c98e4e94df316e829bcd0f6db7ffd5200ac4fe9f
Jérôme Rapinat <romjerome@yahoo.fr>
2009-02-23 18:16:52 +0000

    mistake on names and descriptions (after date)
    svn: r12093

--- src/Filters/Rules/MediaObject/_ChangedSince.py
+++ src/Filters/Rules/MediaObject/_ChangedSince.py
@@ -20,7 +20,7 @@ from Filters.Rules._ChangedSinceBase import ChangedSinceBase
 class ChangedSince(ChangedSinceBase):
     """Rule that checks for media objects changed since a specific time"""
 
-    name        = _('Media objects changed before <date time>')
-    description = _("Matches media objects changed before a specified "
+    name        = _('Media objects changed after <date time>')
+    description = _("Matches media objects changed after a specified "
                     "date:time (yyyy-mm-dd hh:mm:ss) or in the range, if a second "
                     "date:time is given")
