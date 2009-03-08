5d84e3b1e222455680b061cbf2a43aefbc4eb5aa
Doug Blank <doug.blank@gmail.com>
2009-03-08 14:31:20 +0000

    2753: Using 'is' or 'was' logic in the Detailed Reports is flawed; replaced with probably_alive
    svn: r12248

--- src/plugins/textreport/DetDescendantReport.py
+++ src/plugins/textreport/DetDescendantReport.py
@@ -47,6 +47,7 @@ from ReportBase import Bibliography, Endnotes
 import BaseDoc
 import DateHandler
 from BasicUtils import name_displayer as _nd
+import Utils
 
 #------------------------------------------------------------------------
 #
@@ -360,9 +361,9 @@ class DetDescendantReport(Report):
             else:
                 father_name = ""
                 father_mark = ""
-                
+            alive = Utils.probably_alive(person, self.database)
             text = ReportUtils.child_str(person, father_name, mother_name,
-                                         bool(person.get_death_ref()),
+                                         not alive,
                                          firstName,self.verbose)
             if text:
                 self.doc.write_text(text)
