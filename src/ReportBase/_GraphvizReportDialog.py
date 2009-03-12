d48d1122f3d5f5773561291ba5656888b6bd2c96
Peter Landgren <peter.talken@telia.com>
2009-03-12 12:55:29 +0000

    Fix of issue 2790 for nonlatin-1 letters.
    svn: r12328

--- src/ReportBase/_GraphvizReportDialog.py
+++ src/ReportBase/_GraphvizReportDialog.py
@@ -416,7 +416,7 @@ class GVPsDoc(GVDocBase):
         dotfile.close()
         
         # Generate the PS file.
-        os.system( 'dot -Tps2 -o"%s" "%s"' % (self._filename, tmp_dot) )
+        os.system( 'dot -Tps:cairo -o"%s" "%s"' % (self._filename, tmp_dot) )
         
         # Delete the temporary dot file
         os.remove(tmp_dot)
@@ -743,7 +743,7 @@ class GVPdfGsDoc(GVDocBase):
         os.close( handle )
         
         # Generate PostScript using dot
-        command = 'dot -Tps -o"%s" "%s"' % ( tmp_ps, tmp_dot )
+        command = 'dot -Tps:cairo -o"%s" "%s"' % ( tmp_ps, tmp_dot )
         os.system(command)
         
         # Add .5 to remove rounding errors.
