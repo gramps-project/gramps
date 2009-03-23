2f7ae174c0805fb7045aa06083b27c8ed2ab9227
Gerald Britton <gerald.britton@gmail.com>
2009-03-23 15:57:59 +0000

    libhtml.py: change $Id line
    svn: r12382

--- src/plugins/lib/libhtml.py
+++ src/plugins/lib/libhtml.py
@@ -18,7 +18,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-# $Id: html.py 10874 2009-03-03 18:00:00Z gbritton $
+# $Id: libhtml.py 10874 2009-03-03 18:00:00Z gbritton $
 
 #------------------------------------------------------------------------
 #
