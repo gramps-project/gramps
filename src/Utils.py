eb337a8d29792b1997e3afe93d51e239b3df18c0
Benny Malengier <benny.malengier@gramps-project.org>
2009-06-26 09:43:55 +0000

    [Emrys] 2896: Macports 3.0.4 cannot view images
    svn: r12713

--- src/Utils.py
+++ src/Utils.py
@@ -1134,8 +1134,12 @@ def open_file_with_default_application( file_path ):
             ErrorDialog(_("Error Opening File"), str(msg))
     else:
         search = os.environ['PATH'].split(':')
+        if os.sys.platform='darwin':
+            utility = 'open'
+        else:
+            utility = 'xdg-open'
         for lpath in search:
-            prog = os.path.join(lpath, 'xdg-open')
+            prog = os.path.join(lpath, utility)
             if os.path.isfile(prog):
                 os.spawnvpe(os.P_NOWAIT, prog, [prog, norm_path], os.environ)
                 return
