7a235029d80deba7965fad8c34a15727502ba5f3
Jérôme Rapinat <romjerome@yahoo.fr>
2009-07-07 09:44:19 +0000

    typo
    svn: r12773

--- src/Filters/Rules/Place/_ChangedSince.py
+++ src/Filters/Rules/Place/_ChangedSince.py
@@ -20,7 +20,7 @@ from Filters.Rules._ChangedSinceBase import ChangedSinceBase
 class ChangedSince(ChangedSinceBase):
     """Rules that checks for places changed since a specific time."""
 
-    name    = ('Places changed after <date time>')
+    name    = _('Places changed after <date time>')
     description = _("Matches place records changed after a specified "
                     "date-time (yyyy-mm-dd hh:mm:ss) or in the range, if a second "
                     "date-time is given")
