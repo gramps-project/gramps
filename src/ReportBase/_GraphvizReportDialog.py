01492a197470376839193151a17d6d6123103892
Gary Burton <gary.burton@zen.co.uk>
2009-07-10 16:45:22 +0000

    Fix bug causing GraphViz report dialog to crash resulting from recent BaseDoc changes
    svn: r12782

--- src/ReportBase/_GraphvizReportDialog.py
+++ src/ReportBase/_GraphvizReportDialog.py
@@ -153,7 +153,7 @@ class GVDocBase(BaseDoc, GVDoc):
     The close function will generate the actual file of the appropriate type.
     """
     def __init__(self, options, paper_style):
-        BaseDoc.__init__(self, None, paper_style, None)
+        BaseDoc.__init__(self, None, paper_style)
 
         self._filename      = None
         self._dot           = StringIO()
