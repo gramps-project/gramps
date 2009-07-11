aa5fce63a795b956f1356363b7f606d35bd77184
Gary Burton <gary.burton@zen.co.uk>
2009-07-11 19:37:15 +0000

    Remove period character from file format string as it is already supplied by get_ext()
    svn: r12789

--- src/ReportBase/_GraphvizReportDialog.py
+++ src/ReportBase/_GraphvizReportDialog.py
@@ -1091,7 +1091,7 @@ class GraphvizReportDialog(ReportDialog):
             ext = ""
         else:
             spath = self.get_default_directory()
-            base = "%s.%s" % (self.raw_name, ext)
+            base = "%s%s" % (self.raw_name, ext)
             spath = os.path.normpath(os.path.join(spath, base))
             self.target_fileentry.set_filename(spath)
                 
