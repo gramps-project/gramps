61839477635e81f9cb53f98da8e4e1d3076f00af
Doug Blank <doug.blank@gmail.com>
2009-12-12 03:07:19 +0000

    Fixed wrong value for 'track' for detached gramplets
    svn: r13771

--- src/gen/plug/_gramplet.py
+++ src/gen/plug/_gramplet.py
@@ -329,7 +329,7 @@ class Gramplet(object):
         from PluginUtils import make_gui_option
         #tooltips, dbstate, uistate, track
         widget, label = make_gui_option(
-                            option, self.dbstate, self.uistate,None)
+                            option, self.dbstate, self.uistate, [])
         self.option_dict.update({option.get_label(): (widget, option)})
         self.option_order.append(option.get_label())
 
