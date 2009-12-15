c10493e0ad84dc8a3393e32a4bf553256ef56054
Doug Blank <doug.blank@gmail.com>
2009-12-15 16:59:18 +0000

    Moved to gramps-addons
    svn: r13812

--- src/plugins/quickview/quickview.gpr.py
+++ src/plugins/quickview/quickview.gpr.py
@@ -93,25 +93,6 @@ runfunc = 'run_fam'
 
 #------------------------------------------------------------------------
 #
-# All Names of All People
-#
-#------------------------------------------------------------------------
-
-register(QUICKREPORT, 
-id    = 'allnames',
-name  = _("All Names of All People"),
-description =  _("All names of all people"),
-version = '1.0',
-status = STABLE,
-fname = 'AllNames.py',
-authors = ["Douglas Blank"],
-authors_email = ["dblank@cs.brynmawr.edu"],
-category = CATEGORY_QR_PERSON,
-runfunc = 'run'
-  )
-  
-#------------------------------------------------------------------------
-#
 # Relation to Home Person
 #
 #------------------------------------------------------------------------
