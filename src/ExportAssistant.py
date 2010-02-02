9e1e197460b6f6b2c4c40eb445c0f88b091e99be
Benny Malengier <benny.malengier@gramps-project.org>
2010-02-02 08:10:09 +0000

    new gtk version has also a set_window
    svn: r14188

--- src/ExportAssistant.py
+++ src/ExportAssistant.py
@@ -117,7 +117,9 @@ class ExportAssistant(gtk.Assistant, ManagedWindow.ManagedWindow) :
         self.top_title = _("Export Assistant")
         ManagedWindow.ManagedWindow.__init__(self,uistate,[],
                                                  self.__class__)
-        self.set_window(self, None, self.top_title, isWindow=True)
+        #set_window is present in both parent classes
+        ManagedWindow.ManagedWindow.set_window(self, self, None,
+            self.top_title, isWindow=True)
         
         #set up callback method for the export plugins
         self.callback = self.pulse_progressbar
