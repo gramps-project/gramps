ec7a7c869e3ccf4817ab82b8f491b3045ea2a668
Jérôme Rapinat <romjerome@yahoo.fr>
2010-03-03 08:45:42 +0000

    3654: Cannot print title on all_relations quick view
    svn: r14594

--- src/plugins/quickview/all_relations.py
+++ src/plugins/quickview/all_relations.py
@@ -77,8 +77,8 @@ class AllRelReport():
         p1 = self.sdb.name(self.person)
         if self.person.handle == self.home_person.handle :
             self.sdoc.paragraph(_FMT_VOID % (
-                _("%(person)s and %(active_person)s are the same person."))) % {
-                'person' : p1, 'active_person' : p2 } 
+                _("%(person)s and %(active_person)s are the same person.")) % {
+                'person' : p1, 'active_person' : p2 }) 
             return
 
         #check if not a family too:
@@ -203,8 +203,8 @@ class AllRelReport():
         """
         p2 = self.sdb.name(self.home_person)
         p1 = self.sdb.name(self.person)
-        self.sdoc.title(_("Relationships of %(person)s to %(active_person)s")) % {
-            'person' : p1 ,'active_person' : p2 }
+        self.sdoc.title(_("Relationships of %(person)s to %(active_person)s") % {
+            'person' : p1 ,'active_person' : p2 })
         self.sdoc.paragraph("")
 
     def print_details_header(self, relations, pers1, pers2, 
