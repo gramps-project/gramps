56c898064d4283fe0b05bd131f232257d367cb0a
Doug Blank <doug.blank@gmail.com>
2010-03-07 16:08:12 +0000

    Added FAQs with links from user list and wiki
    svn: r14665

--- src/plugins/gramplet/FaqGramplet.py
+++ src/plugins/gramplet/FaqGramplet.py
@@ -35,9 +35,19 @@ class FAQGramplet(Gramplet):
     def init(self):
         self.set_use_markup(True)
         self.clear_text()
-        self.render_text("Draft of a <a wiki='FAQ'>Frequently Asked Questions</a> Gramplet\n\n")
-        self.render_text("  1. <a href='http://bugs.gramps-project.org/'>Test 1</a>\n")
-        self.render_text("  2. <a href='http://gramps-project.org//'>Test 2</a>\n")
+        self.render_text(_("<a wiki='Gramps_3.2_Wiki_Manual_-_FAQ'>Frequently Asked Questions</a> (need connection to internet)\n\n"))
+        self.render_text(_("  1. <a wiki='Gramps_3.2_Wiki_Manual_-_FAQ#How_do_I_change_the_order_of_spouses.3F'>How do I change the order of spouses?</a>\n"))
+        self.render_text(_("  2. <a wiki='Gramps_3.2_Wiki_Manual_-_FAQ#How_do_I_upgrade_GRAMPS.3F'>Is it necessary to update Gramps every time an update is released?</a>\n"))
+        self.render_text(_("  3. <a wiki='Gramps_3.2_Wiki_Manual_-_FAQ#How_do_I_keep_backups.3F'>How do I make backups safely?</a>\n"))
+        self.render_text(_("  4. <a wiki='Gramps_3.2_Wiki_Manual_-_Entering_and_Editing_Data:_Detailed_-_part_1#Editing_Information_About_Relationships'>How should information about marriages be entered?</a>\n"))
+        self.render_text(_("  5. <a wiki='Gramps_3.2_Wiki_Manual_-_FAQ#What_is_the_difference_between_a_residence_and_an_address.3F'>What's the difference between a residence and an address?</a>\n"))
+        self.render_text(_("  6. <a wiki='Gramps_3.2_Wiki_Manual_-_FAQ#How_can_I_publish_web_sites_generated_by_GRAMPS.3F'>How can I make a website with Gramps and my tree?</a>\n"))
+        self.render_text(_("  7. <a href='http://old.nabble.com/German-translation-of-%22occupation%22-ts21786114.html#a21786114'>How do I record one's occupation?</a>\n"))
+        self.render_text(_("  8. <a wiki='Gramps_3.2_Wiki_Manual_-_FAQ#What_do_I_do_if_I_have_found_a_bug.3F'>What do I do if I have found a bug?</a>\n"))
+        self.render_text(_("  9. <a wiki='Portal:Using_GRAMPS'>Is there a manual for Gramps?</a>\n"))
+        self.render_text(_(" 10. <a wiki='Category:Tutorials'>Are there tutorials available?</a>\n"))
+        self.render_text(_(" 11. <a wiki='Category:How_do_I...'>How do I ...?</a>\n"))
+        self.render_text(_(" 12. <a wiki='How_you_can_help'>How can I help with Gramps?</a>\n"))
 
     def post_init(self):
         self.disconnect("active-changed")
