54b87d8fc62f3254ea2737a69b5595b93fd14ef8
Doug Blank <doug.blank@gmail.com>
2010-04-18 12:25:51 +0000

    Fixed error in losing options on config of this gramplet
    svn: r15210

--- src/plugins/gramplet/QuickViewGramplet.py
+++ src/plugins/gramplet/QuickViewGramplet.py
@@ -119,7 +119,6 @@ class QuickViewGramplet(Gramplet):
                     "Repository": CATEGORY_QR_REPOSITORY}
         qv_option = self.get_option(_("View Type"))
         list_option = self.get_option(_("Quick Views"))
-        list_option.clear()
         qv_list = get_quick_report_list(code_map[qv_option.get_value()])
         for pdata in qv_list:
             list_option.add_item(pdata.id, pdata.name)
