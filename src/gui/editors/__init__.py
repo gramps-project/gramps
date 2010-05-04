98436e2e53ade7991af2b8e15fae2bd0a2d9b90b
Doug Blank <doug.blank@gmail.com>
2010-05-04 22:13:43 +0000

    Double Oops: that line does belong here
    svn: r15321

--- src/gui/editors/__init__.py
+++ src/gui/editors/__init__.py
@@ -37,6 +37,7 @@ from editrepository import EditRepository, DeleteRepositoryQuery
 from editreporef import EditRepoRef
 from editsource import EditSource, DeleteSrcQuery
 from editsourceref import EditSourceRef
+from editurl import EditUrl
 
 # Map from gen.obj name to Editor:
 EDITORS = {
