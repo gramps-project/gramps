3d19491f406e28278912d0f0a593c2a270bc6acc
Nick Hall <nick__hall@hotmail.com>
2010-08-08 18:33:21 +0000

    Sort by "surname" instead of "group as" in flat person view
    svn: r15686

--- src/gui/views/treemodels/peoplemodel.py
+++ src/gui/views/treemodels/peoplemodel.py
@@ -184,7 +184,7 @@ class PeopleBaseModel(object):
     def sort_name(self, data):
         n = Name()
         n.unserialize(data[COLUMN_NAME])
-        return (n.get_group_name(), n.get_first_name())
+        return (n.get_surname(), n.get_first_name())
 
     def column_name(self, data):
         handle = data[0]
