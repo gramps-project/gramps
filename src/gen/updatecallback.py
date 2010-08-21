01e354f18d08bf5d27396a40d3d023a707eb609f
Doug Blank <doug.blank@gmail.com>
2010-08-21 12:31:31 +0000

    Make sure newvalue is a int(fraction * 100)
    svn: r15784

--- src/gen/updatecallback.py
+++ src/gen/updatecallback.py
@@ -78,7 +78,7 @@ class UpdateCallback(object):
         self.count += 1
         if not count:
             count = self.count
-        newval = int(100*count/self.total)
+        newval = int(100 * count/float(self.total))
         newtime = time.time()
         time_has_come = self.interval and (newtime-self.oldtime>self.interval)
         value_changed = newval!=self.oldval
