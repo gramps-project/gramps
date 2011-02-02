bced98bd711cb1157c0d34b91c1bb8d238951b90
Nick Hall <nick__hall@hotmail.com>
2011-02-02 22:09:14 +0000

    4591: Do not allow less than a single column in the Gramplet view
    svn: r16546

--- src/gui/widgets/grampletpane.py
+++ src/gui/widgets/grampletpane.py
@@ -1102,6 +1102,8 @@ class GrampletPane(gtk.ScrolledWindow):
         return True
 
     def set_columns(self, num):
+        if num < 1:
+            num = 1
         # clear the gramplets:
         self.clear_gramplets()
         # clear the columns:
