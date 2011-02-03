60e8c0c013731bcd05ffba2061106adedf7e1774
Benny Malengier <benny.malengier@gramps-project.org>
2011-02-03 22:23:36 +0000

    3975: Accessing an incompatible database corrupts the database Undoing unset of DB_RECOVERY flag, we need it to have ACI(D) database
    svn: r16562

--- src/gen/db/write.py
+++ src/gen/db/write.py
@@ -423,6 +423,9 @@ class DbBsddb(DbBsddbRead, DbWriteBase, UpdateCallback):
                     db.DB_INIT_MPOOL | db.DB_INIT_LOCK |\
                     db.DB_INIT_LOG | db.DB_INIT_TXN | db.DB_THREAD
 
+        # As opposed to before, we always try recovery on databases
+        env_flags |= db.DB_RECOVER
+
         # Environment name is now based on the filename
         env_name = name
 
