45b99dabd21036803dcbc7ae8e9c4eda1ce912fe
Michiel Nauta <m.d.nauta@hetnet.nl>
2011-02-22 08:04:04 +0000

    4666: GEDCOM import into a black database crashes trunk, OK with 3.2.5
    svn: r16692

--- src/plugins/lib/libgedcom.py
+++ src/plugins/lib/libgedcom.py
@@ -2282,8 +2282,8 @@ class GedcomParser(UpdateCallback):
         Parses the opened GEDCOM file.
         """
         no_magic = self.maxpeople < 1000
-        with DbTxn(_("GEDCOM import"), self.dbase, not use_trans, no_magic
-                   ) as self.trans:
+        with DbTxn(_("GEDCOM import"), self.dbase, not use_trans,
+                   no_magic=no_magic) as self.trans:
 
             self.dbase.disable_signals()
             self.__parse_header_head()
