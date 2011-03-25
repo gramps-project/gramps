6a7dd3db9024184da309f67f5b9195be70d2d494
Rob G. Healey <robhealey1@gmail.com>
2011-03-25 03:51:47 +0000

    Swappped out MetadataViewer Gramplet in MediaView bottombar for Exif from Nick Hall.
    svn: r16914

--- src/plugins/view/mediaview.py
+++ src/plugins/view/mediaview.py
@@ -437,7 +437,7 @@ class MediaView(ListView):
         """
         return (("Media Filter Gramplet",),
                 ("Media Preview Gramplet",
-                 "Metadata Viewer Gramplet",
+                 "Exif",
                  "Media Sources Gramplet",
                  "Media Notes Gramplet",
                  "Media Attributes Gramplet"))
