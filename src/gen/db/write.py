bdf97fabd0419b14e07b0018c9886b71eb04efca
Michiel Nauta <m.d.nauta@hetnet.nl>
2011-05-02 20:10:37 +0000

    4322: Upgrading a large database to new version runs out of memory; increase locktable size
    svn: r17385

--- src/gen/db/write.py
+++ src/gen/db/write.py
@@ -1016,6 +1016,11 @@ class DbBsddb(DbBsddbRead, DbWriteBase, UpdateCallback):
             return
         self.env.txn_checkpoint()
 
+        lockstats = self.env.lock_stat()
+        _LOG.debug("lock occupancy: %d%%, locked object occupancy: %d%%" % (
+            round(lockstats['maxnlocks']*100/lockstats['maxlocks']),
+            round(lockstats['maxnobjects']*100/lockstats['maxobjects'])))
+
         self.__close_metadata()
         self.name_group.close()
         self.surnames.close()
