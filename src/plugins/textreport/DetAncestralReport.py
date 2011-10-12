5001870f8da3ae75f04eda5658f8a367d92af4db
Doug Blank <doug.blank@gmail.com>
2011-10-12 23:10:41 +0000

    Python 2.6.4 could not deepcopy the name_display
    svn: r18306

--- src/plugins/textreport/DetAncestralReport.py
+++ src/plugins/textreport/DetAncestralReport.py
@@ -34,6 +34,7 @@
 #
 #------------------------------------------------------------------------
 import copy
+import sys
 from gen.ggettext import gettext as _
 
 #------------------------------------------------------------------------
@@ -140,7 +141,12 @@ class DetAncestorReport(Report):
 
         # Copy the global NameDisplay so that we don't change application 
         # defaults.
-        self._name_display = copy.deepcopy(global_name_display)
+        try:
+            self._name_display = copy.deepcopy(global_name_display)
+        except:
+            print >> sys.stderr, "WARNING: unable to deepcopy name_display; continuing..."
+            self._name_display = global_name_display
+            
         name_format = menu.get_option_by_name("name_format").get_value()
         if name_format != 0:
             self._name_display.set_default_format(name_format)
