22dd294bc4ef2494c1862f3147f7196d1623b58b
Rob G. Healey <robhealey1@gmail.com>
2012-01-25 04:52:29 +0000

    Adding Cascading Style Sheet for the beginning of HTML5/ CSS3 Drop Down menus option in NarrativeWeb.
    svn: r18768

--- src/plugins/webstuff/webstuff.py
+++ src/plugins/webstuff/webstuff.py
@@ -126,6 +126,10 @@ def load_on_reg(dbstate, uistate, plugin):
         ["Fade-Menus", 0, "", 
          path_css('Web_Fade-Menus.css'), None, [], [] ],
 
+        # webkit/Html5/CSS3 Drop Down Menus Style Sheet
+        ["DropDown-Menus", 0, "",
+         path_css("Web_DropDown-Menus.css"), None, [], [] ],
+
         # Vertical Navigation Menus Style Sheet
         ["Vertical-Menus", 0, "", 
          path_css('Web_Vertical-Menus.css'), None, [], [] ],
