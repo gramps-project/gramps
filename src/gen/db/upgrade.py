5daa484bce28bd8c86e79b1b40cabd83d7a778fb
Tim G L Lyons <tim.g.lyons@gmail.com>
2012-02-02 12:53:24 +0000

    Change to use monospaced font in InfoDialog in upgrade 16 report. (The report was designed for a monospaced font)
    svn: r18812

--- src/gen/db/upgrade.py
+++ src/gen/db/upgrade.py
@@ -421,7 +421,7 @@ def gramps_upgrade_16(self):
              "Tools -> Family Tree Processing -> Merge\n"
              "in order to merge citations that contain similar\n"
              "information")
-    InfoDialog(_('Upgrade Statistics'), txt)
+    InfoDialog(_('Upgrade Statistics'), txt, monospaced=True)
 
 def upgrade_media_list_16(self, media_list):
     new_media_list = []
