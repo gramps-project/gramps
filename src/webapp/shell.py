396f51754848f0892e7410eb584e5424a17c16c9
Doug Blank <doug.blank@gmail.com>
2012-05-27 19:00:50 +0000

    Refinements on interactive shell
    svn: r19677

--- src/webapp/shell.py
+++ src/webapp/shell.py
@@ -1,3 +1,9 @@
+#### This sets up Django so you can interact with it via the Python
+#### command line.
+#### Start with something like:
+####    $ PYTHONPATH=.. python -i shell.py 
+####    >>> Person.objects.all()
+
 from django.conf import settings
 import webapp.settings as default_settings
 try:
@@ -13,12 +19,13 @@ from webapp.reports import import_file
 from webapp.libdjango import DjangoInterface, totime, todate
 from gen.datehandler import displayer, parser
 import gen.lib
+import cli.user
 
 db = DbDjango()
 dji = DjangoInterface()
 dd = displayer.display
 dp = parser.parse
 
-#def Print(m):
-#    print m
-#import_file(db, "/tmp/dblank-im_ged.ged", Print)
+#import_file(db, 
+#            "/home/dblank/gramps/trunk/example/gramps/data.gramps", 
+#            cli.user.User())
