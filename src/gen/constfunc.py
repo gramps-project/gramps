e9a25208023b6517547f07ad70aac9a717e43ee1
John Ralls <jralls@ceridwen.us>
2012-07-15 23:49:11 +0000

    Convert backend determination for pygi and Gtk3 (GEP-029)
    svn: r20005

--- src/gen/constfunc.py
+++ src/gen/constfunc.py
@@ -94,7 +94,7 @@ def is_quartz():
             from gi.repository import Gdk
         except:
             return False
-        return Gdk.WINDOWING == "quartz"
+        return Gdk.Display.get_default().__class__.__name__.endswith("quartzDisplay")
     return False
 
 def has_display():
