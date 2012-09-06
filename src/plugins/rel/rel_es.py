aaa0b1966efc43dee7daa6b5087dc6ca5c55c057
Julio Sánchez <julio.sanchez@gmail.com>
2012-09-06 12:54:54 +0000

    Fix error in plural cousins (same generation) relationship string generation
    svn: r20342

--- src/plugins/rel/rel_es.py
+++ src/plugins/rel/rel_es.py
@@ -600,7 +600,7 @@ class RelationshipCalculator(Relationship.RelationshipCalculator):
             # These are cousins in the same generation
             if Ga == 2:
                 rel_str = "primos hermanos"
-            elif level < len(_level_name_plural):
+            elif (Ga-1) < len(_level_name_plural):
                 rel_str = "primos %s" % (_level_name_plural[Ga-1])
             else:
                 rel_str = "primos %d-ésimos" % (Ga-1)
