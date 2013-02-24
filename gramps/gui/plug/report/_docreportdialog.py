4621970783cb1b5511637ecd1aea839973610f64
Paul Franklin <pf98052@gmail.com>
2013-02-24 08:39:51 +0000

    revert to the old report name
    svn: r21428

--- gramps/gui/plug/report/_docreportdialog.py
+++ gramps/gui/plug/report/_docreportdialog.py
@@ -185,9 +185,7 @@ class DocReportDialog(ReportDialog):
             ext = ""
         else:
             spath = self.get_default_directory()
-            default_name = self.dbname + "_" + \
-                        "".join(x[0].upper() for x in self.raw_name.split("_"))
-            base = "%s.%s" % (default_name, ext)
+            default_name = self.dbname + "_" + self.raw_name
             if self.options.get_output():
                 base = os.path.basename(self.options.get_output())
             else:
--- gramps/gui/plug/report/_graphvizreportdialog.py
+++ gramps/gui/plug/report/_graphvizreportdialog.py
@@ -166,12 +166,11 @@ class GraphvizReportDialog(ReportDialog):
             ext = ""
         else:
             spath = self.get_default_directory()
-            default_name = self.dbname + "_" + \
-                        "".join(x[0].upper() for x in self.raw_name.split("_"))
+            default_name = self.dbname + "_" + self.raw_name
             if self.options.get_output():
                 base = os.path.basename(self.options.get_output())
             else:
-                base = "%s.%s" % (default_name, ext)
+                base = "%s%s" % (default_name, ext) # "ext" already has a dot
             spath = os.path.normpath(os.path.join(spath, base))
             self.target_fileentry.set_filename(spath)
                 
