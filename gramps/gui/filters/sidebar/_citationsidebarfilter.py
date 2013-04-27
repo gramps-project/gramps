a0cae8afcfd98f4000fbe5882c45e25b677e2be8
Jérôme Rapinat <romjerome@yahoo.fr>
2013-04-27 16:35:27 +0000

    5751: use "Minimum Confidence" label into citation sidebar filter, it does not match only one level
    svn: r22109

--- gramps/gui/filters/sidebar/_citationsidebarfilter.py
+++ gramps/gui/filters/sidebar/_citationsidebarfilter.py
@@ -27,7 +27,7 @@
 #
 #-------------------------------------------------------------------------
 from gramps.gen.const import GRAMPS_LOCALE as glocale
-_ = glocale.translation.gettext
+_ = glocale.translation.sgettext
 
 #-------------------------------------------------------------------------
 #
@@ -97,7 +97,7 @@ class CitationSidebarFilter(SidebarFilter):
         self.add_text_entry(_('ID'), self.filter_id)
         self.add_text_entry(_('Volume/Page'), self.filter_page)
         self.add_text_entry(_('Date'), self.filter_date)
-        self.add_entry(_('Confidence'), self.filter_conf)
+        self.add_entry(_('Minimum Confidence|Min. Conf.'), self.filter_conf)
         self.add_text_entry(_('Note'), self.filter_note)
         self.add_filter_entry(_('Custom filter'), self.generic)
         self.add_entry(None, self.filter_regex)
