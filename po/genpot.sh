5e42724282e27027eb38080a56bf3deca2d980fe
Jérôme Rapinat <romjerome@yahoo.fr>
2013-05-05 12:54:13 +0000

    6595: missing custom keywords support for intltool
    svn: r22178

--- po/genpot.sh
+++ po/genpot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# Make translation files
-intltool-update -g gramps -o gramps.pot -p
+# Make translation files (keywords issues, see bug #6595)
+# intltool-update -g gramps -o gramps.pot -p
