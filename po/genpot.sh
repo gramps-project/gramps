757a6a721b221c6fb2f30f05b2195363724f0f72
Paul Franklin <pf98052@gmail.com>
2013-05-05 19:40:56 +0000

    add new keywords to po/genpot.sh, comment both scripts, and add "Id" lines
    svn: r22182

--- po/genpot.sh
+++ po/genpot.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-# Make translation files (keywords issues, see bug #6595)
-# intltool-update -g gramps -o gramps.pot -p
+# $Id$
+
+# Make translation files
+
+# additional keywords must always be kept in sync with those in update_po.py
+XGETTEXT_ARGS='--keyword=_T_ --keyword=trans_text_ --keyword=_ngettext' \
+  intltool-update -g gramps -o gramps.pot -p
