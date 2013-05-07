f31c22cdaa11d638ed895a1b9eb098b6f89f95c6
Paul Franklin <pf98052@gmail.com>
2013-05-07 18:12:23 +0000

    "_ngettext" is no longer used as a gramps translation keyword
    svn: r22198

--- po/update_po.py
+++ po/update_po.py
@@ -582,7 +582,7 @@ def retrieve():
     os.system('''%(xgettext)s -j --directory=./ -d gramps '''
               '''-L Python -o gramps.pot --files-from=python.txt '''
               '''--keyword=_ --keyword=ngettext '''
-              '''--keyword=_T_ --keyword=trans_text_ --keyword=_ngettext '''
+              '''--keyword=_T_ --keyword=trans_text_ '''
               '''--keyword=sgettext --from-code=UTF-8''' % {'xgettext': xgettextCmd}
              )
     
