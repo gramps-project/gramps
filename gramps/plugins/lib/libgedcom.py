076fdac5a782763ed6bc55a497fae69b7e8a1e7b
Tim G L Lyons <tim.g.lyons@gmail.com>
2013-05-14 15:42:42 +0000

    0006541: Gramps crashes during import gedcom. Ignore empty ALIA alias name.
    svn: r22298

--- gramps/plugins/lib/libgedcom.py
+++ gramps/plugins/lib/libgedcom.py
@@ -3572,7 +3572,10 @@ class GedcomParser(UpdateCallback):
         @param state: The current state
         @type state: CurrentState
         """
-        if line.data[0] == '@':
+        if line.data == '':
+            self.__add_msg(_("Empty Alias <NAME PERSONAL> ignored"), line, state)
+            self.__skip_subordinate_levels(state.level+1, state)
+        elif line.data[0] == '@':
             handle = self.__find_person_handle(self.pid_map[line.data])
             ref = PersonRef()
             ref.ref = handle
