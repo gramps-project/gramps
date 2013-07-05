485798010d4d386f3ef562cb91539afad2a2f112
Serge Noiraud <Serge.Noiraud@free.fr>
2013-07-05 18:08:30 +0000

    Geography : add Id property to cairoprint.py
    svn: r22654

--- gramps/plugins/lib/maps/cairoprint.py
+++ gramps/plugins/lib/maps/cairoprint.py
@@ -18,7 +18,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-# $Id: print.py 21985 2013-04-13 14:37:34Z noirauds $
+# $Id$
 
 #-------------------------------------------------------------------------
 #
