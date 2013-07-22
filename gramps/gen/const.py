8b1f6d7def315bb9365a993295f414f789381e82
Vassilii Khachaturov <vassilii@tarunz.org>
2013-07-22 12:04:22 +0000

    6932: Check for broken GTK install on linux only
    Fix as proposed by jralls.
    Also, move the 'gtk30' translation domain name to gramps consts.
    
    svn: r22717

--- gramps/gen/const.py
+++ gramps/gen/const.py
@@ -194,6 +194,7 @@ LICENSE_FILE = os.path.join(_resources.doc_dir, 'COPYING')
 from gramps.gen.utils.grampslocale import GrampsLocale
 GRAMPS_LOCALE = GrampsLocale(localedir=_resources.locale_dir)
 _ = GRAMPS_LOCALE.translation.sgettext
+GTK_GETTEXT_DOMAIN = 'gtk30'
 
 #-------------------------------------------------------------------------
 #
