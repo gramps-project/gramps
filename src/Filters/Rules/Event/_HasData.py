gramps-project/gramps

commit b596db78b4b0b8b750bb8db0a746167b614b5a88

Backport regular expression enhancements from gramps40
git-svn-id: svn+ssh://svn.code.sf.net/p/gramps/code/branches/maintenance/gramps34@22837 4ae1f11a-8b86-4847-b8af-ab372f36d1fd

src/Filters/Rules/Event/_HasData.py
@@ -49,29 +49,21 @@ class HasData(Rule):
     name        = _('Events with <data>')
     description = _("Matches events with data of a particular value")
     category    = _('General filters')
+    allow_regex = True
     
-    def __init__(self, list, use_regex=False):
-        Rule.__init__(self, list, use_regex)
-
+    def prepare(self, dbase):
         self.event_type = self.list[0]
         self.date = self.list[1]
-        self.place = self.list[2]
-        self.description = self.list[3]
 
         if self.event_type:
             self.event_type = EventType()
             self.event_type.set_from_xml_str(self.list[0])
 
         if self.date:
-            self.date = DateHandler.parser.parse(self.date)
+            self.date = parser.parse(self.date)
         
     def apply(self, db, event):
         if self.event_type and event.get_type() != self.event_type:
-            # No match
-            return False
-
-        ed = event.get_description().upper()
-        if self.description and ed.find(self.description.upper()) == -1:
             # No match
             return False
 
@@ -79,17 +71,20 @@ class HasData(Rule):
             # No match
             return False
 
-        if self.place:
-            pl_id = event.get_place_handle()
-            if pl_id:
-                pl = db.get_place_from_handle(pl_id)
-                pn = pl.get_title().upper()
-                if pn.find(self.place.upper()) == -1:
+        if self.list[2]:
+            place_id = event.get_place_handle()
+            if place_id:
+                place = db.get_place_from_handle(place_id)
+                if not self.match_substring(2, place.get_title()):
                     # No match
                     return False
             else:
                 # No place attached to event
                 return False
+
+        if not self.match_substring(3, event.get_description()):
+            # No match
+            return False
 
         # All conditions matched
         return True

src/Filters/Rules/Person/_HasBirth.py
@@ -48,11 +48,11 @@ class HasBirth(Rule):
     name        = _('People with the <birth data>')
     description = _("Matches people with birth data of a particular value")
     category    = _('Event filters')
+    allow_regex = True
     
-    def __init__(self, list, use_regex=False):
-        Rule.__init__(self, list, use_regex)
+    def prepare(self, db):
         if self.list[0]:
-            self.date = DateHandler.parser.parse(self.list[0])
+            self.date = parser.parse(self.list[0])
         else:
             self.date = None
         
@@ -67,9 +67,7 @@ class HasBirth(Rule):
             if event.get_type() != EventType.BIRTH:
                 # No match: wrong type
                 continue
-            ed = event.get_description().upper()
-            if self.list[2] \
-                   and ed.find(self.list[2].upper())==-1:
+            if not self.match_substring(2, event.get_description()):
                 # No match: wrong description
                 continue
             if self.date:
@@ -77,11 +75,10 @@ class HasBirth(Rule):
                     # No match: wrong date
                     continue
             if self.list[1]:
-                pl_id = event.get_place_handle()
-                if pl_id:
-                    pl = db.get_place_from_handle(pl_id)
-                    pn = pl.get_title().upper()
-                    if pn.find(self.list[1].upper()) == -1:
+                place_id = event.get_place_handle()
+                if place_id:
+                    place = db.get_place_from_handle(place_id)
+                    if not self.match_substring(1, place.get_title()):
                         # No match: wrong place
                         continue
                 else:

src/Filters/Rules/Person/_HasDeath.py
@@ -48,11 +48,11 @@ class HasDeath(Rule):
     name        = _('People with the <death data>')
     description = _("Matches people with death data of a particular value")
     category    = _('Event filters')
+    allow_regex = True
     
-    def __init__(self, list, use_regex=False):
-        Rule.__init__(self, list, use_regex)
+    def prepare(self, db):
         if self.list[0]:
-            self.date = DateHandler.parser.parse(self.list[0])
+            self.date = parser.parse(self.list[0])
         else:
             self.date = None
 
@@ -67,9 +67,7 @@ class HasDeath(Rule):
             if event.get_type() != EventType.DEATH:
                 # No match: wrong type
                 continue
-            ed = event.get_description().upper()
-            if self.list[2] \
-                   and ed.find(self.list[2].upper())==-1:
+            if not self.match_substring(2, event.get_description()):
                 # No match: wrong description
                 continue
             if self.date:
@@ -77,11 +75,10 @@ class HasDeath(Rule):
                     # No match: wrong date
                     continue
             if self.list[1]:
-                pl_id = event.get_place_handle()
-                if pl_id:
-                    pl = db.get_place_from_handle(pl_id)
-                    pn = pl.get_title().upper()
-                    if pn.find(self.list[1].upper()) == -1:
+                place_id = event.get_place_handle()
+                if place_id:
+                    place = db.get_place_from_handle(place_id)
+                    if not self.match_substring(1, place.get_title()):
                         # No match: wrong place
                         continue
                 else:

src/Filters/Rules/Person/_HasFamilyEvent.py
@@ -52,12 +52,13 @@ class HasFamilyEvent(Rule):
     name        =  _('People with the family <event>')
     description = _("Matches people with a family event of a particular value")
     category    = _('Event filters')
+    allow_regex = True
     
     def prepare(self,db):
         self.date = None
         try:
             if self.list[1]:
-                self.date = DateHandler.parser.parse(self.list[1])
+                self.date = parser.parse(self.list[1])
         except:
             pass
 
@@ -77,18 +78,17 @@ class HasFamilyEvent(Rule):
                     specified_type.set_from_xml_str(self.list[0])
                     if event.type != specified_type:
                         val = 0
-                v = self.list[3]
-                if v and event.get_description().upper().find(v.upper())==-1:
-                    val = 0
+                if self.list[3]:
+                    if not self.match_substring(3, event.get_description()):
+                        val = 0
                 if self.date:
                     if event.get_date_object().match(self.date):
                         val = 0
                 if self.list[2]:
-                    pl_id = event.get_place_handle()
-                    if pl_id:
-                        pl = db.get_place_from_handle(pl_id)
-                        pn = pl.get_title().upper()
-                        if pn.find(self.list[2].upper()) == -1:
+                    place_id = event.get_place_handle()
+                    if place_id:
+                        place = db.get_place_from_handle(place_id)
+                        if not self.match_substring(2, place.get_title()):
                             val = 0
                     else:
                         val = 0

src/Filters/Rules/Citation/_HasNoteRegexp.py
@@ -40,6 +40,6 @@ from Filters.Rules._HasNoteRegexBase import HasNoteRegexBase
 #-------------------------------------------------------------------------
 class HasNoteRegexp(HasNoteRegexBase):
 
-    name        = _('Citations having notes containing <regular expression>')
+    name        = _('Citations having notes containing <text>')
     description = _("Matches citations whose notes contain text "
                     "matching a regular expression")

src/Filters/Rules/Citation/_MatchesPageSubstringOf.py
@@ -40,15 +40,13 @@ from Filters.Rules import Rule
 class MatchesPageSubstringOf(Rule):
     """Citation Volume/Page title containing <substring>"""
 
-    labels      = [ _('Substring:')]
-    name        = _('Citation Volume/Page containing <substring>')
+    labels      = [ _('Text:')]
+    name        = _('Citations with Volume/Page containing <text>')
     description = _("Matches citations whose Volume/Page contains a "
                     "certain substring")
     category    = _('General filters')
+    allow_regex = True
 
     def apply(self, db, object):
         """ Apply the filter """
-        title = object.get_page()
-        if title.upper().find(self.list[0].upper()) != -1:
-            return True
-        return False
+        return self.match_substring(0, object.get_page())

src/Filters/Rules/Citation/_RegExpIdOf.py
@@ -46,6 +46,6 @@ class RegExpIdOf(RegExpIdBase):
     matches regular expression.
     """
 
-    name        = _('Citations with <Id> matching regular expression')
+    name        = _('Citations with Id containing <text>')
     description = _("Matches citations whose Gramps ID matches "
                     "the regular expression")

src/Filters/Rules/Citation/__init__.py
@@ -51,7 +51,6 @@ editor_rule_list = [
     HasGallery,
     HasIdOf,
     HasNote,
-    HasNoteMatchingSubstringOf,
     HasNoteRegexp,
     HasReferenceCountOf,
     HasSource,

commit 024629c2e1e1f17afac4e540c3549446d30864a8

Fix bug introduced in r22837
svn: r22879

src/Filters/Rules/Event/_HasData.py
@@ -60,7 +60,7 @@ class HasData(Rule):
             self.event_type.set_from_xml_str(self.list[0])
 
         if self.date:
-            self.date = parser.parse(self.date)
+            self.date = DateHandler.parser.parse(self.date)
         
     def apply(self, db, event):
         if self.event_type and event.get_type() != self.event_type:

src/Filters/Rules/Person/_HasBirth.py
@@ -52,7 +52,7 @@ class HasBirth(Rule):
     
     def prepare(self, db):
         if self.list[0]:
-            self.date = parser.parse(self.list[0])
+            self.date = DateHandler.parser.parse(self.list[0])
         else:
             self.date = None
         

src/Filters/Rules/Person/_HasDeath.py
@@ -52,7 +52,7 @@ class HasDeath(Rule):
     
     def prepare(self, db):
         if self.list[0]:
-            self.date = parser.parse(self.list[0])
+            self.date = DateHandler.parser.parse(self.list[0])
         else:
             self.date = None
 

src/Filters/Rules/Person/_HasFamilyEvent.py
@@ -58,7 +58,7 @@ class HasFamilyEvent(Rule):
         self.date = None
         try:
             if self.list[1]:
-                self.date = parser.parse(self.list[1])
+                self.date = DateHandler.parser.parse(self.list[1])
         except:
             pass
 