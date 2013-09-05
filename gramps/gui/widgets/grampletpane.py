66fc6893ea90758258551f0dce5bd07f2fc874be
Nick Hall <nick__hall@hotmail.com>
2013-09-05 22:47:02 +0000

    Fix bug listing gramplets already in gramplet bar
    svn: r23041

--- gramps/gui/widgets/grampletpane.py
+++ gramps/gui/widgets/grampletpane.py
@@ -112,9 +112,9 @@ def GET_AVAILABLE_GRAMPLETS(name):
     return None
 
 def GET_GRAMPLET_LIST(nav_type, skip):
-    return [(gplug.gramplet_title, gplug.id) 
+    return [(gplug.gramplet_title, gplug.id)
             for gplug in PLUGMAN.get_reg_gramplets()
-            if gplug.navtypes == [] or nav_type in gplug.navtypes
+            if (gplug.navtypes == [] or nav_type in gplug.navtypes)
             and gplug.name not in skip]
 
 def parse_tag_attr(text):
