a0db80f643e53ca1f3fd4fe8bad3135f2986d09a
Serge Noiraud <Serge.Noiraud@free.fr>
2013-09-09 15:34:53 +0000

    Geography : bug#0006930 : incorrect string in geoplaces
    svn: r23066

--- src/plugins/view/geoplaces.py
+++ src/plugins/view/geoplaces.py
@@ -113,7 +113,7 @@ class GeoPlaces(GeoGraphyView):
     """
 
     def __init__(self, pdata, dbstate, uistate, nav_group=0):
-        GeoGraphyView.__init__(self, _('Places places map'),
+        GeoGraphyView.__init__(self, _('Places map'),
                                       pdata, dbstate, uistate, 
                                       dbstate.db.get_place_bookmarks(), 
                                       Bookmarks.PlaceBookmarks,
