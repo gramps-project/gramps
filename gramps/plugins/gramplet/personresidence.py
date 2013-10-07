918be872309d8e7537d75bc38396d387fa3a0496
Nick Hall <nick__hall@hotmail.com>
2013-10-07 22:03:12 +0000

    Freeze headings when scrolling
    svn: r23266

--- gramps/plugins/gramplet/personresidence.py
+++ gramps/plugins/gramplet/personresidence.py
@@ -36,7 +36,7 @@ class PersonResidence(Gramplet):
     def init(self):
         self.gui.WIDGET = self.build_gui()
         self.gui.get_container_widget().remove(self.gui.textview)
-        self.gui.get_container_widget().add_with_viewport(self.gui.WIDGET)
+        self.gui.get_container_widget().add(self.gui.WIDGET)
         self.gui.WIDGET.show()
 
     def build_gui(self):
