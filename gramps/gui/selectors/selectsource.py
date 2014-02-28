3d7901fec46ee175abbbd0af4581bc66a5fd271f
Nick Hall <nick__hall@hotmail.com>
2014-02-28 12:20:22 +0000

    Add author column to source selector

--- gramps/gui/selectors/selectsource.py
+++ gramps/gui/selectors/selectsource.py
@@ -60,8 +60,9 @@ class SelectSource(BaseSelector):
     def get_column_titles(self):
         return [
             (_('Title'), 350, BaseSelector.TEXT, 0),
-            (_('ID'),     75, BaseSelector.TEXT, 1),
-            (_('Last Change'),       150, BaseSelector.TEXT, 7),
+            (_('Author'), 200, BaseSelector.TEXT, 2),
+            (_('ID'), 75, BaseSelector.TEXT, 1),
+            (_('Last Change'), 150, BaseSelector.TEXT, 7),
             ]
 
     def get_from_handle_func(self):
