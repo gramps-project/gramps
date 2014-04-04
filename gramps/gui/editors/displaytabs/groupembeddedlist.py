2353c0bd34b52283ff27179b44c11150cf691dcc
Nick Hall <nick__hall@hotmail.com>
2014-04-04 19:30:10 +0100

    1772: Select row on right click in embedded lists

--- gramps/gui/editors/displaytabs/groupembeddedlist.py
+++ gramps/gui/editors/displaytabs/groupembeddedlist.py
@@ -109,6 +109,7 @@ class GroupEmbeddedList(EmbeddedList):
         """
         Handle button press, not double-click, that is done in init_interface
         """
+        self._select_row_at_coords(event.x, event.y)
         if is_right_click(event):
             obj = self.get_selected()
             if obj and obj[1]:
