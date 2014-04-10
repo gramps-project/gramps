dd1a3f5e159af408c45ffcab8d4793a674f30d97
Nick Hall <nick__hall@hotmail.com>
2014-04-10 18:12:18 +0100

    Fix family editor callback

--- gramps/gui/editors/editfamily.py
+++ gramps/gui/editors/editfamily.py
@@ -344,7 +344,8 @@ class EditFamily(EditPrimary):
         
         EditPrimary.__init__(self, dbstate, uistate, track,
                              family, dbstate.db.get_family_from_handle,
-                             dbstate.db.get_family_from_gramps_id)
+                             dbstate.db.get_family_from_gramps_id,
+                             callback)
 
         # look for the scenerio of a child and no parents on a new
         # family
@@ -1106,6 +1107,9 @@ class EditFamily(EditPrimary):
                     self.db.commit_family(self.obj, trans)
 
         self._do_close()
+        if self.callback:
+            self.callback(self.obj)
+        self.callback = None
 
     def no_name(self):
         """
