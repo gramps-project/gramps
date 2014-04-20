aaa12567623f3abb39b2fd24bd1bb19217dd66c9
John Ralls <jralls@ceridwen.us>
2014-04-19 17:49:25 -0700

    7258: A consequence of forcing paths to unicode is that we sometimes need to explicitly decode a path to utf8 to pass to Gtk
    Only in Py2, of course, since the Py3 version of PyGObject knows what to do.

--- gramps/gui/glade.py
+++ gramps/gui/glade.py
@@ -49,7 +49,7 @@ from gi.repository import Gtk
 #
 #------------------------------------------------------------------------
 from gramps.gen.const import GLADE_DIR, GRAMPS_LOCALE as glocale
-from gramps.gen.constfunc import STRTYPE
+from gramps.gen.constfunc import STRTYPE, uni_to_gui
 
 #------------------------------------------------------------------------
 #
@@ -124,7 +124,7 @@ class Glade(Gtk.Builder):
         
         # try to build Gtk objects from glade file.  Let exceptions happen
         
-        self.add_from_file(path)
+        self.add_from_file(uni_to_gui(path))
         self.__dirname, self.__filename = os.path.split(path)
         
         # try to find the toplevel widget
