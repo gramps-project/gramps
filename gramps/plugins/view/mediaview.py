9046ea8de5e5506a1b94ad28920f38cd09768f32
Jérôme Rapinat <romjerome@yahoo.fr>
2014-07-15 20:40:18 +0200

    7618: Cannot drag and drop external media object with a non-ascii filename or path; missing conv_to_unicode (typo)

--- gramps/plugins/view/mediaview.py
+++ gramps/plugins/view/mediaview.py
@@ -60,7 +60,7 @@ from gi.repository import Gtk
 from gramps.gui.utils import open_file_with_default_application
 from gramps.gui.views.listview import ListView, TEXT, MARKUP, ICON
 from gramps.gui.views.treemodels import MediaModel
-from gramps.gen.constfunc import win, cuni
+from gramps.gen.constfunc import win, cuni, conv_to_unicode
 from gramps.gen.config import config
 from gramps.gen.utils.file import (media_path, relative_path, media_path_full)
 from gramps.gen.utils.db import get_media_referents
