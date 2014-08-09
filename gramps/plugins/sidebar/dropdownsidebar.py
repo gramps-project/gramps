86303f6116a40e14081ea7276ac9702b92b86fdb
Malcom Lewis <unknown@gramps.org>
2014-08-08 19:29:07 -0700

    Update FSF address to current location.

--- gramps/plugins/sidebar/dropdownsidebar.py
+++ gramps/plugins/sidebar/dropdownsidebar.py
@@ -19,7 +19,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 # $Id: categorysidebar.py 20634 2012-11-07 17:53:14Z bmcage $
 
