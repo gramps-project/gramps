55c7becb68c2314d5a2c3a80d7a454a4efed991a
Nick Hall <nick__hall@hotmail.com>
2015-02-02 17:32:15 +0000

    Update import to cope with missing place name and type

--- gramps/plugins/importer/importxml.py
+++ gramps/plugins/importer/importxml.py
@@ -1150,8 +1150,9 @@ class GrampsParser(UpdateCallback):
         self.placeobj.private = bool(attrs.get("priv"))
         self.placeobj.change = int(attrs.get('change', self.change))
         if self.__xml_version >= (1, 6, 0):
-            self.placeobj.name = attrs.get("name")
-            self.placeobj.place_type.set_from_xml_str(attrs.get("type"))
+            self.placeobj.name = attrs.get('name', '')
+            if 'type' in attrs:
+                self.placeobj.place_type.set_from_xml_str(attrs.get('type'))
         self.info.add('new-object', PLACE_KEY, self.placeobj)
         
         # GRAMPS LEGACY: title in the placeobj tag
