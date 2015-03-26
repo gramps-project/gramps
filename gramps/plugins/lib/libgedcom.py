2a1da2efba7402952a3757cac5383fb25b8cf515
kulath <guy.linton@gmail.com>
2015-03-26 12:19:26 +0000

    Fix bug in processing of Place FORM in GEDCOM import as a result of fix for 8233.
    When an unused Place was removed, PlaceImport still tried to generate
    the place hierarchy. Also fixed bug in GEDCOM import where a default
    PLAC FORM in the GEDCOM header was ignored.

--- gramps/plugins/lib/libgedcom.py
+++ gramps/plugins/lib/libgedcom.py
@@ -5372,6 +5372,7 @@ class GedcomParser(UpdateCallback):
                             self.place_names[title].append(place_handle)
                         else:
                             place.merge(old_place)
+                            self.place_import.remove_location(old_place.handle)
                             self.dbase.remove_place(place_handle, self.trans)
                             self.place_names[old_title].remove(place_handle)
                     else:
@@ -5392,7 +5393,7 @@ class GedcomParser(UpdateCallback):
             sub_state = CurrentState()
             sub_state.place = place
             sub_state.level = state.level+1
-            sub_state.pf = PlaceParser()
+            sub_state.pf = self.place_parser
 
             self.__parse_level(sub_state, self.event_place_map, 
                              self.__undefined)
@@ -5538,6 +5539,7 @@ class GedcomParser(UpdateCallback):
                         self.__add_location(place, location)
                     else:
                         place.merge(old_place)
+                        self.place_import.remove_location(old_place.handle)
                         self.dbase.remove_place(place_handle, self.trans)
                         self.place_names[title].remove(place_handle)
                 else:
