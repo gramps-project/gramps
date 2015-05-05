774855496fbfe0ceba93168b5db0919c1f33b1af
Doug Blank <dblank@cs.brynmawr.edu>
2015-05-05 13:25:20 -0400

    8541: Crash following update: addon permission issue blocks re-starting gramps
    There were two issues:
    
    1. attempting to read a new addon file that wasn't readable
       threw an exception, aborted updating addons
    
    2. global error catching didn't properly handle error code
       from an OSError

--- gramps/gen/plug/_pluginreg.py
+++ gramps/gen/plug/_pluginreg.py
@@ -1096,7 +1096,13 @@ class PluginRegister(object):
                 continue
             lenpd = len(self.__plugindata)
             full_filename = os.path.join(dir, filename)
-            fd = io.open(full_filename, "r", encoding='utf-8')
+            try:
+                fd = io.open(full_filename, "r", encoding='utf-8')
+            except Exception as msg:
+                print(_('ERROR: Failed reading plugin registration %(filename)s') % \
+                            {'filename' : filename})
+                print(msg)
+                continue
             stream = fd.read()
             fd.close()
             if os.path.exists(os.path.join(os.path.dirname(full_filename),
