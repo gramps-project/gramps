75009f0fe0ed6ecdc575c522f6a14a9d6262a666
Nick Hall <nick-h@gramps-project.org>
2015-05-21 20:54:30 +0100

    Convert deprecated stock items in glade files

--- gramps/gui/dbman.py
+++ gramps/gui/dbman.py
@@ -126,7 +126,7 @@ class DbManager(CLIDbManager):
         the GTK widgets that are needed.
         """
         CLIDbManager.__init__(self, dbstate)
-        self.glade = Glade()
+        self.glade = Glade(toplevel='dbmanager')
         self.top = self.glade.toplevel
 
         if parent:
