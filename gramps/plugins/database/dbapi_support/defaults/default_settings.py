7bcb6295471629c5c524dab2bdf0d62257bec6ff
Doug Blank <dblank@cs.brynmawr.edu>
2015-06-02 10:55:38 -0400

    Updated backends to 5.0; moved Django and DBAPI to addons

--- gramps/plugins/database/dbapi_support/defaults/default_settings.py
+++
@@ -1,7 +0,0 @@
-import os
-import sqlite3
-
-path_to_db = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                        'sqlite.db')  
-dbapi = sqlite3.connect(path_to_db)
-dbapi.row_factory = sqlite3.Row # allows access by name
