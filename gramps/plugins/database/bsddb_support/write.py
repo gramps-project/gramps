ecc078b891a08581d7d63323e28fa3f1cdf4676a
Doug Blank <dblank@cs.brynmawr.edu>
2015-08-22 20:06:14 -0400

    DbTxn: use no_magic explicitly

--- gramps/plugins/database/bsddb_support/write.py
+++ gramps/plugins/database/bsddb_support/write.py
@@ -2430,14 +2430,14 @@ class DbBsddb(DbBsddbRead, DbWriteBase, UpdateCallback):
         """
         return DbTxn
 
-    def DbTxn(self, message, batch=False, **kwargs):
+    def DbTxn(self, message, batch=False, no_magic=False):
         """
         Create a transaction for this database type.
 
         kwargs may contain:
            no_magic
         """
-        return DbTxn(message, self, batch, **kwargs)
+        return DbTxn(message, self, batch, no_magic)
 
     def backup(self):
         """
