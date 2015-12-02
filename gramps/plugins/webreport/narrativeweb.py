75a4a380a926093d754747bd9a2f65317933177c
SNoiraud <serge.noiraud@laposte.net>
2015-12-03 00:47:11 +0100

    9650 : permission denied : change mtime to origin instead of destination.

--- gramps/plugins/webreport/narrativeweb.py
+++ gramps/plugins/webreport/narrativeweb.py
@@ -550,7 +550,7 @@ def copy_thumbnail(report, handle, photo, region=None):
                                        photo.get_mime_type(),
                                        region)
         mtime = os.stat(full_path).st_mtime
-        os.utime(from_path, (mtime, mtime))
+        os.utime(to_path, (mtime, mtime))
         if not os.path.isfile(from_path):
             from_path = CSS["Document"]["filename"]
     else:
