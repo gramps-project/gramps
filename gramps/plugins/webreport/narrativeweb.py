b9d3378020266ed82ee352c3c246502e5cf6c5f5
SNoiraud <serge.noiraud@laposte.net>
2015-12-04 11:11:16 +0100

    8950 : narrativeweb : change mtime at the bad place and made twice.

--- gramps/plugins/webreport/narrativeweb.py
+++ gramps/plugins/webreport/narrativeweb.py
@@ -549,8 +549,6 @@ def copy_thumbnail(report, handle, photo, region=None):
         from_path = get_thumbnail_path(full_path,
                                        photo.get_mime_type(),
                                        region)
-        mtime = os.stat(full_path).st_mtime
-        os.utime(to_path, (mtime, mtime))
         if not os.path.isfile(from_path):
             from_path = CSS["Document"]["filename"]
     else:
