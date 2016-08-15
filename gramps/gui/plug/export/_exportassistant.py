3af32ec258fac1adf95a03a175c67cb42556fe8d
SNoiraud <serge.noiraud@laposte.net>
2016-08-15 10:19:34 +0200

    8250: Gramps crashes when closed while exporting

--- gramps/gui/plug/export/_exportassistant.py
+++ gramps/gui/plug/export/_exportassistant.py
@@ -119,7 +119,8 @@ class ExportAssistant(Gtk.Assistant, ManagedWindow) :
 
         #set_window is present in both parent classes
         ManagedWindow.set_window(self, self, None,
-            self.top_title, isWindow=True)        
+            self.top_title, isWindow=True)
+        self.set_modal(True)
 
         #set up callback method for the export plugins
         self.callback = self.pulse_progressbar
