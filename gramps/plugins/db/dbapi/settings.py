a1dff4457b00b6c52976f63f98657c4251121d7d
romjerome <romjerome@users.noreply.github.com>
2016-12-21 12:02:43 +0100

    9824: pylint error: explicit os module import
    was pull request #284

--- gramps/plugins/db/dbapi/settings.py
+++ gramps/plugins/db/dbapi/settings.py
@@ -17,6 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+import os
 
 ## This file is copied from gramps/plugins/db/dbapi/settings.py
 ## into each grampsdb/*/ directory. You can edit each copy
