b8495e7436e202708683e65bf5caa6b0ae24eef5
Nick Hall <nick-h@gramps-project.org>
2017-01-09 19:02:49 +0000

    Revert "fix warnings on import order (pylint)"
    This reverts commit 86c4301cb9d6b8ad6b7222146099022b44b3d9df.

--- gramps/gen/const.py
+++ gramps/gen/const.py
@@ -39,11 +39,11 @@ import uuid
 # Gramps modules
 #
 #-------------------------------------------------------------------------
+from .git_revision import get_git_revision
+from .constfunc import get_env_var
 from gramps.version import VERSION, VERSION_TUPLE, major_version
 from gramps.gen.utils.resourcepath import ResourcePath
 from gramps.gen.utils.grampslocale import GrampsLocale
-from .git_revision import get_git_revision
-from .constfunc import get_env_varGrampsLocale
 
 #-------------------------------------------------------------------------
 #
