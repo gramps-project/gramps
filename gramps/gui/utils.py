fe763b7490c69a331f3931a488294549c3ee29e3
romjerome <romjerome@users.noreply.github.com>
2017-01-14 10:01:42 +0100

    9899: polish; gen.lib.person arlready called, make test on gender more clear

--- gramps/gui/utils.py
+++ gramps/gui/utils.py
@@ -700,9 +700,9 @@ def parents_labels(db, family):
     if len(family.get_child_ref_list()) > 0:
         rel_father = _('Father')
         rel_mother = _('Mother')
-        if father.gender == 0:
+        if father.gender == Person.FEMALE:
             rel_father = rel_mother
-        if mother.gender == 1:
+        if mother.gender == Person.MALE:
             rel_mother = _('Father')
     else:
         rc = RelationshipCalculator()
