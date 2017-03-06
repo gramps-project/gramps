c1c2c1557c44ac4a21ec222ee39638ed0ba4cfd5
Nick Hall <nick-h@gramps-project.org>
2017-03-06 23:27:07 +0000

    Suppress output in importgeneweb test

--- gramps/plugins/importer/test/importgeneweb_test.py
+++ gramps/plugins/importer/test/importgeneweb_test.py
@@ -22,6 +22,7 @@
 Unit test of the GeneWebParser
 """
 import unittest
+import logging
 from ..importgeneweb import GeneWebParser
 from ....gen.lib.date import Date
 
@@ -35,6 +36,8 @@ class ParseDateTest(unittest.TestCase):
 
     def test_invalid_date_stays_verbatim_text(self):
         text = "29/2/1875"
+        logging.disable(logging.WARNING)
         d = self.parse(text)
+        logging.disable(logging.NOTSET)
         self.assertEqual(d.get_modifier(), Date.MOD_TEXTONLY)
         self.assertEqual(d.get_text(), text)
