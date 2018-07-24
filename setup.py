3c5c34d0c5237c0ac4969eb1e6351b805f267561
Nick Hall <nick-h@gramps-project.org>
2018-07-24 14:20:41 +0100

    Update development status for new release

--- setup.py
+++ setup.py
@@ -493,7 +493,7 @@ setup(name = 'gramps',
       data_files = data_files,
       scripts = ['scripts/gramps'],
       classifiers = [
-          "Development Status :: 4 - Beta",
+          "Development Status :: 5 - Production/Stable",
           "Environment :: Console",
           "Environment :: MacOS X",
           "Environment :: Plugins",
