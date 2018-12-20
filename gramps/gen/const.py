cb8bf0d00a2e836ddb0943c3c1e5d5937cdca522
prculley <paulr2787@gmail.com>
2018-12-20 11:16:55 -0600

    Release Gramps 5.0.1

--- gramps/gen/const.py
+++ gramps/gen/const.py
@@ -138,7 +138,7 @@ sys.path.insert(0, ROOT_DIR)
 git_revision = get_git_revision(ROOT_DIR).replace('\n', '')
 if sys.platform == 'win32' and git_revision == "":
     git_revision = get_git_revision(os.path.split(ROOT_DIR)[1])
-VERSION += git_revision
+#VERSION += git_revision
 
 #
 # Glade files
