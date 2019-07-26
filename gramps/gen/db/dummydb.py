c10d14615ca435585861454344d96d86bafac2f7
romjerome <romjerome@users.noreply.github.com>
2019-07-26 14:20:43 +0200

    fix trailing whitespace

--- gramps/gen/db/dummydb.py
+++ gramps/gen/db/dummydb.py
@@ -756,7 +756,7 @@ class DummyDb(M_A_M_B("NewBaseClass", (DbReadBase, Callback, object,), {})):
         if not self.db_is_open:
             LOG.warning("database is closed")
         return 0
-    
+
     def get_number_of_citations(self):
         """
         Return the number of citations currently in the database.
