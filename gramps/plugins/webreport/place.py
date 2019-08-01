96fcef2a84273d231b45203c54f9d43e6256fba8
Serge Noiraud <Serge.Noiraud@laposte.net>
2019-08-01 15:31:39 +0200

    Narrative web: Sort problem with places. (#854)
    * Narrative web: Sort problem with places.
    
    Fixes #10868.
    
    Places list is not sorted depending on the selected language.
    
    If you start gramps in english or another language then try to generated
    a narrative web report in another language, the navigation alphabet is
    incorrect. This is true for the place list and the person list.
    The problem was related to pyICU.
    
    * Narrative web: some pylint improvement.

--- gramps/plugins/webreport/place.py
+++ gramps/plugins/webreport/place.py
@@ -64,7 +64,7 @@ from gramps.plugins.webreport.common import (get_first_letters, first_letter,
                                              primary_difference, _KEYPLACE,
                                              get_index_letter, FULLCLEAR,
                                              MARKER_PATH, OSM_MARKERS, MARKERS,
-                                             html_escape)
+                                             html_escape, sort_places)
 
 _ = glocale.translation.sgettext
 LOG = logging.getLogger(".NarrativeWeb")
@@ -181,21 +181,15 @@ class PlacePages(BasePage):
                     ]
                 )
 
-                # bug 9495 : incomplete display of place hierarchy labels
-                def sort_by_place_name(obj):
-                    """ sort by lower case place name. """
-                    name = self.report.obj_dict[Place][obj][1]
-                    return name.lower()
-
-                handle_list = sorted(place_handles,
-                                     key=lambda x: sort_by_place_name(x))
+                handle_list = sort_places(self.r_db, place_handles,
+                                          self.rlocale)
                 first = True
 
                 # begin table body
                 tbody = Html("tbody")
                 table += tbody
 
-                for place_handle in handle_list:
+                for (dummy_pname, place_handle) in handle_list:
                     place = self.r_db.get_place_from_handle(place_handle)
                     if place:
                         if place.get_change_time() > ldatec:
