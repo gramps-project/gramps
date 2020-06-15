f290fd9bcf0e0197501d7998569651a28ae981ff
vantu5z <vantu5z@mail.ru>
2020-06-15 13:59:33 +0300

    Use a contrasting text color in pedigree view
    White text shows up better in boxes with a dark background color.
    
    Fixes #11799

--- gramps/gui/utils.py
+++ gramps/gui/utils.py
@@ -545,6 +545,15 @@ def rgb_to_hex(rgb):
         rgbint = (int(rgb[0] * 255), int(rgb[1] * 255), int(rgb[2] * 255))
         return '#%02x%02x%02x' % rgbint
 
+def get_contrast_color(color):
+    """
+    Choose contrast text color (white or black) for provided background.
+    """
+    yiq = (color[0]*299)+(color[1]*587)+(color[2]*114)
+    if (yiq < 500):
+        return (1, 1, 1)  # 'white'
+    return (0, 0, 0)      # 'black'
+
 def get_link_color(context):
     """
     Find the link color for the current theme.
