a867149b2cd5eb94f9fb33bf7a7228deaea8a4ff
SNoiraud <serge.noiraud@laposte.net>
2020-09-28 21:12:10 +0200

    Add a scroll to top button.

--- gramps/plugins/webreport/basepage.py
+++ gramps/plugins/webreport/basepage.py
@@ -111,6 +111,32 @@ function toggleContent(elem, icon) {
 </script>
 """
 
+GOTOTOP = """
+<script>
+//Get the button
+var gototop = document.getElementById("gototop");
+
+// When the user scrolls down 200px from the top of the document,
+// show the button
+window.onscroll = function() {scroll()};
+
+function scroll() {
+  if (document.body.scrollTop > 200 ||
+      document.documentElement.scrollTop > 200) {
+    gototop.style.display = "block";
+  } else {
+    gototop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function GoToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
+"""
+
 class BasePage:
     """
     Manages all the functions, variables, and everything needed
@@ -1650,6 +1676,15 @@ class BasePage:
 
         if self.report.options['toggle']:
             head += TOGGLE
+
+        # Create a button to go to the top of the page
+        viewbox = "0 0 100 100"
+        points = "0,100 100,100, 50,10"
+        svg = Html("svg", viewBox=viewbox, class_="triangle", inline=False)
+        svg += Html("polygon", points=points)
+        outerwrapperdiv += Html("button", svg, id="gototop", title=_("Go to top"), onclick="GoToTop()")
+        outerwrapperdiv += GOTOTOP # This must be positioned
+                                   # after the button for it to work
 
         # message for Codacy :
         # body is used in some modules to add functions like onload(),
