0acf57e04b5505c0a0dde7b6909f0c307d740aa1
Stanislav <42420548+PushKK@users.noreply.github.com>
2021-06-05 11:12:18 +0300

    Update toolsdebug.gpr.py
    Add localization, for https://github.com/gramps-project/gramps/pull/1251

--- gramps/plugins/tool/toolsdebug.gpr.py
+++ gramps/plugins/tool/toolsdebug.gpr.py
@@ -35,12 +35,12 @@ MODULE_VERSION="5.2"
 
 register(TOOL,
 id = 'test_for_date_parser_and_displayer',
-name = "Check Localized Date Displayer and Parser",
-description = ("This test tool will create many people showing all"
-                " different date variants as birth. The death date is"
-                " created by parsing the result of the date displayer for"
-                " the birth date. This way you can ensure that dates"
-                " printed can be parsed back in correctly."),
+name = _("Check Localized Date Displayer and Parser"),
+description = _("This test tool will create many people showing all "
+                "different date variants as birth. The death date is "
+                "created by parsing the result of the date displayer for "
+                "the birth date. This way you can ensure that dates "
+                "printed can be parsed back in correctly."),
 version = '1.0',
 gramps_target_version = MODULE_VERSION,
 status = UNSTABLE,
@@ -84,8 +84,8 @@ tool_modes = [TOOL_MODE_GUI, TOOL_MODE_CLI]
 
 register(TOOL,
 id = 'testcasegenerator',
-name = "Generate Testcases for Persons and Families",
-description = ("The testcase generator will generate some persons "
+name = _("Generate Testcases for Persons and Families"),
+description = _("The testcase generator will generate some persons "
                 "and families that have broken links in the database "
                 "or data that is in conflict to a relation."),
 version = '1.0',
@@ -108,8 +108,8 @@ tool_modes = [TOOL_MODE_GUI, TOOL_MODE_CLI]
 
 register(TOOL,
 id = 'populatesources',
-name = "Populate Sources and Citations",
-description = ("This tool generates sources and citations for each source in "
+name = _("Populate Sources and Citations"),
+description = _("This tool generates sources and citations for each source in "
                 "order to populate the database for testing with significant "
                 "numbers of sources and citations."),
 version = '1.0',
