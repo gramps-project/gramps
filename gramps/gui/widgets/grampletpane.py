8eed6b869a1f75dbdb66f84473668687fdacd7f6
SNoiraud <serge.noiraud@laposte.net>
2021-09-04 12:00:04 +0200

    Solve InterpolationSyntaxError if "%" in a string
    The grampletpane module save data in a config file for all
    the gramplets added in the dashboard.
    The python configparser module doesn't like if we have a "%"
    character in a string.
    
    Fixes #012423

--- gramps/gui/widgets/grampletpane.py
+++ gramps/gui/widgets/grampletpane.py
@@ -1239,6 +1239,12 @@ class GrampletPane(Gtk.ScrolledWindow):
                                     else:
                                         cnt = 0
                                         for item in base_opts["data"]:
+                                            # If we have a "%" in a string,
+                                            # escape it by writing "%%"
+                                            # to avoid InterpolationSyntaxError
+                                            # in python configparser module.
+                                            if isinstance(item, str):
+                                                item = item.replace("%", "%%")
                                             fp.write("data[%d]=%s\n" % (cnt, item))
                                             cnt += 1
                                 else:
