e4ba28cca3516ab184afba3d55043fefdb726933
romjerome <romjerome@users.noreply.github.com>
2022-02-20 18:36:45 +0100

    fix missing active_handle

--- gramps/gui/selectors/selectsource.py
+++ gramps/gui/selectors/selectsource.py
@@ -92,8 +92,12 @@ class SelectSource(BaseSelector):
             gid = marked.get_gramps_id()
             sfilter.add_rule(rules.source.HasIdOf([gid]))
 
-        BaseSelector.__init__(self, dbstate, uistate, track, sfilter,
-                              skip, show_search_bar, active_handle)
+        if active_handle:
+            BaseSelector.__init__(self, dbstate, uistate, track, sfilter,
+                                  skip, show_search_bar, active_handle)
+        else:
+            BaseSelector.__init__(self, dbstate, uistate, track, sfilter,
+                                  skip, show_search_bar)
 
     def _local_init(self):
         """
