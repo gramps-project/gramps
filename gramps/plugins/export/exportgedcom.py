80305b1ab10159a0b980c33aa93ba85c029cbd70
Nick Hall <nick-h@gramps-project.org>
2023-05-13 15:10:20 +0100

    Fix export where private citations are excluded

--- gramps/plugins/export/exportgedcom.py
+++ gramps/plugins/export/exportgedcom.py
@@ -1344,6 +1344,8 @@ class GedcomWriter(UpdateCallback):
         """
 
         citation = self.dbase.get_citation_from_handle(citation_handle)
+        if citation is None: # removed by proxy
+            return
 
         src_handle = citation.get_reference_handle()
         if src_handle is None:
