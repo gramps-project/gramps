33a9dff2bfe92b2a6cf2ba558e864b1f6a1270b0
Nick Hall <nick-h@gramps-project.org>
2023-08-26 17:42:35 +0100

    A second attempt to fix translator comments

--- gramps/gen/datehandler/_datedisplay.py
+++ gramps/gen/datehandler/_datedisplay.py
@@ -494,12 +494,12 @@ class DateDisplay:
                 date_type = _("", "after-date")
             elif mod == Date.MOD_FROM:
                 # Translators: If there is no special inflection for
-                # "after <Month>" in your language, DON'T translate this.
+                # "from <Month>" in your language, DON'T translate this.
                 # Otherwise, translate to "from" in ENGLISH!!! ENGLISH!!!
                 date_type = _("", "from-date")
             elif mod == Date.MOD_TO:
                 # Translators: If there is no special inflection for
-                # "after <Month>" in your language, DON'T translate this.
+                # "to <Month>" in your language, DON'T translate this.
                 # Otherwise, translate to "to" in ENGLISH!!! ENGLISH!!!
                 date_type = _("", "to-date")
             elif mod == Date.MOD_ABOUT:
