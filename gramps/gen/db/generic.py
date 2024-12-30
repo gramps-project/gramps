de7460c151c2c4187fa23039c4251c4415eb1b51
Doug Blank <doug.blank@gmail.com>
2024-12-30 08:06:42 -0500

    Linting

--- gramps/gen/db/generic.py
+++ gramps/gen/db/generic.py
@@ -2739,4 +2739,3 @@ class DbGeneric(DbWriteBase, DbReadBase, UpdateCallback, Callback):
             self.serializer = BlobSerializer
         elif serializer_name == "json":
             self.serializer = JSONSerializer
-
