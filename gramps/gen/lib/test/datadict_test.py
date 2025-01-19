ae36f1e5eadc4f279da772731cda8350c64c1f6f
Doug Blank <doug.blank@gmail.com>
2025-01-19 08:12:54 -0500

    Moved asserts to TestCase.asserts

--- gramps/gen/lib/test/datadict_test.py
+++ gramps/gen/lib/test/datadict_test.py
@@ -36,41 +36,41 @@ class DataDictTest(unittest.TestCase):
         p = Person()
         d = DataDict(p)
         # Get the object from the dict:
-        assert id(p) == id(from_dict(d))
+        self.assertEqual(id(p), id(from_dict(d)))
 
     def test_person_to_dict(self):
         p = Person()
         d = to_dict(p)
-        assert not hasattr(d, "_object")
+        self.assertFalse(hasattr(d, "_object"))
 
     def test_family(self):
         p = Family()
         d = DataDict(p)
         # Get the object from the dict:
-        assert id(p) == id(from_dict(d))
+        self.assertEqual(id(p), id(from_dict(d)))
 
     def test_family_to_dict(self):
         p = Family()
         d = to_dict(p)
-        assert not hasattr(d, "_object")
+        self.assertFalse(hasattr(d, "_object"))
 
 
 class DataListTest(unittest.TestCase):
     def test_empty_1(self):
         dl = DataList()
-        assert isinstance(dl, DataList)
-        assert len(dl) == 0
+        self.assertIsInstance(dl, DataList)
+        self.assertEqual(len(dl), 0)
 
     def test_empty_2(self):
         dl = DataList([])
-        assert isinstance(dl, DataList)
-        assert len(dl) == 0
+        self.assertIsInstance(dl, DataList)
+        self.assertEqual(len(dl), 0)
 
     def test_value_1(self):
         dl = DataList([42])
-        assert isinstance(dl, DataList)
-        assert isinstance(dl[0], int)
-        assert dl[0] == 42
+        self.assertIsInstance(dl, DataList)
+        self.assertIsInstance(dl[0], int)
+        self.assertEqual(dl[0], 42)
 
     def test_value_exception(self):
         with self.assertRaises(TypeError):
@@ -78,44 +78,44 @@ class DataListTest(unittest.TestCase):
 
     def test_access_exception(self):
         dl = DataList([1, 2, 3])
-        assert isinstance(dl, DataList)
-        assert dl[0] == 1
-        assert dl[1] == 2
-        assert dl[2] == 3
+        self.assertIsInstance(dl, DataList)
+        self.assertEqual(dl[0], 1)
+        self.assertEqual(dl[1], 2)
+        self.assertEqual(dl[2], 3)
         with self.assertRaises(IndexError):
             dl[3]
 
     def test_nested(self):
         dl = DataList([[42]])
-        assert isinstance(dl, DataList)
-        assert isinstance(dl[0], DataList)
-        assert dl[0][0] == 42
+        self.assertIsInstance(dl, DataList)
+        self.assertIsInstance(dl[0], DataList)
+        self.assertEqual(dl[0][0], 42)
 
     def test_dict(self):
         p = Person()
         p_dict = to_dict(p)
         dl = DataList([p_dict])
-        assert isinstance(dl, DataList)
-        assert isinstance(dl[0], DataDict)
-        assert dl[0] == p_dict
-        assert dl[0].gender == 2
+        self.assertIsInstance(dl, DataList)
+        self.assertIsInstance(dl[0], DataDict)
+        self.assertEqual(dl[0], p_dict)
+        self.assertEqual(dl[0].gender, 2)
 
     def test_append_list(self):
         dl = DataList([])
         dl.append([])
-        assert isinstance(dl, DataList)
-        assert dl[0] == []
-        assert isinstance(dl[0], DataList)
+        self.assertIsInstance(dl, DataList)
+        self.assertEqual(dl[0], [])
+        self.assertIsInstance(dl[0], DataList)
 
     def test_append_value(self):
         dl = DataList([])
         dl.append(42)
-        assert isinstance(dl, DataList)
-        assert dl[0] == 42
-        assert isinstance(dl[0], int)
+        self.assertIsInstance(dl, DataList)
+        self.assertEqual(dl[0], 42)
+        self.assertIsInstance(dl[0], int)
 
     def test_combined_list(self):
         # FIXME: dealt with in a later PR
         dl = DataList([])
         with self.assertRaises(AssertionError):
-            assert isinstance(dl + [], DataList)
+            self.assertIsInstance(dl + [], DataList)
