42c3157f9afd19b9376f510bdb8ad35bef858197
Douglas Blank <doug.blank@gmail.com>
2025-01-25 12:04:20 -0500

    Update gramps/gen/lib/serialize.py
    Co-authored-by: stevenyoungs <steve@youngs.cc>

--- gramps/gen/lib/serialize.py
+++ gramps/gen/lib/serialize.py
@@ -114,8 +114,7 @@ class DataDict(dict):
         return self["_object"].__gt__(value)
 
     def __le__(self, value):
-        if "_object" not in self:
-            self["_object"] = from_dict(self)
+        self.__get_object()
 
         if isinstance(value, DataDict):
             value = value._object
