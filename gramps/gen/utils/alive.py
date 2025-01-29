a85d8a46f31bd77b65ca29f85d80f5c048537b92
CameronD73 <56762299+CameronD73@users.noreply.github.com>
2025-01-29 18:48:57 +1000

    removed unreachable else statement

--- gramps/gen/utils/alive.py
+++ gramps/gen/utils/alive.py
@@ -1001,8 +1001,6 @@ def probably_alive(
     )
     if current_date is None or not current_date.is_valid():
         current_date = Today()
-    elif not current_date.is_valid():
-        current_date = Today()
 
     if DEBUGLEVEL > 3:
         if relative is None:
