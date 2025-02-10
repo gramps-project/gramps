974359bc7887c27a70e6f096c2b5d6215811f9d2
Doug Blank <doug.blank@gmail.com>
2025-02-10 14:07:47 -0500

    WIP: outlining sections of code

--- gramps/gen/proxy/proxybase.py
+++ gramps/gen/proxy/proxybase.py
@@ -54,6 +54,12 @@ class ProxyDbBase:
         self.proxy_map = {}
         self.proxy_map["Person"] = self.get_person_map()
         self.proxy_map["Family"] = self.get_family_map()
+        self.proxy_map["Event"] = self.get_event_map()
+        self.proxy_map["Place"] = self.get_place_map()
+        self.proxy_map["Repository"] = self.get_repository_map()
+        self.proxy_map["Media"] = self.get_media_map()
+        self.proxy_map["Citation"] = self.get_citation_map()
+        self.proxy_map["Note"] = self.get_note_map()
 
         self.bookmarks = DbBookmarks(
             self.filter_person_handles(self.db.bookmarks.get())
@@ -78,6 +84,8 @@ class ProxyDbBase:
         finally:
             locale.setlocale(locale.LC_ALL, old_locale)
 
+    # Get maps:
+
     def get_person_map(self):
         # Get a map from map[handle] = {"surname": "Name"}
         map = {}
@@ -97,16 +105,95 @@ class ProxyDbBase:
             map[handle] = {"surname": "TODO"}
         return map
 
+    def get_event_map(self):
+        map = {}
+        for handle in self.db.iter_event_handles():
+            map[handle] = {}
+        return map
+
+    def get_place_map(self):
+        map = {}
+        for handle in self.db.iter_place_handles():
+            map[handle] = {}
+        return map
+
+    def get_repository_map(self):
+        map = {}
+        for handle in self.db.iter_repository_handles():
+            map[handle] = {}
+        return map
+
+    def get_media_map(self):
+        map = {}
+        for handle in self.db.iter_media_handles():
+            map[handle] = {}
+        return map
+
+    def get_citation_map(self):
+        map = {}
+        for handle in self.db.iter_citation_handles():
+            map[handle] = {}
+        return map
+
+    def get_note_map(self):
+        map = {}
+        for handle in self.db.iter_note_handles():
+            map[handle] = {}
+        return map
+
+    # Proxy process:
+
     def proxy_process_person(self, person):
         return person
 
     def proxy_process_family(self, family):
         return family
 
+    def proxy_process_event(self, event):
+        return event
+
+    def proxy_process_place(self, place):
+        return place
+
+    def proxy_process_repository(self, repository):
+        return repository
+
+    def proxy_process_media(self, media):
+        return media
+
+    def proxy_process_citation(self, citation):
+        return citation
+
+    def proxy_process_note(self, note):
+        return note
+
+    # Filter handles:
+
     def filter_person_handles(self, handles):
         return [handle for handle in handles if handle in self.proxy_map["Person"]]
 
-    # The public methods:
+    def filter_family_handles(self, handles):
+        return [handle for handle in handles if handle in self.proxy_map["Family"]]
+
+    def filter_event_handles(self, handles):
+        return [handle for handle in handles if handle in self.proxy_map["Event"]]
+
+    def filter_place_handles(self, handles):
+        return [handle for handle in handles if handle in self.proxy_map["Place"]]
+
+    def filter_repository_handles(self, handles):
+        return [handle for handle in handles if handle in self.proxy_map["Repository"]]
+
+    def filter_media_handles(self, handles):
+        return [handle for handle in handles if handle in self.proxy_map["Media"]]
+
+    def filter_citation_handles(self, handles):
+        return [handle for handle in handles if handle in self.proxy_map["Citation"]]
+
+    def filter_note_handles(self, handles):
+        return [handle for handle in handles if handle in self.proxy_map["Note"]]
+
+    # Iter raw data:
 
     def _iter_raw_person_data(self):
         for handle in self.proxy_map["Person"]:
@@ -114,11 +201,15 @@ class ProxyDbBase:
             person = self.get_person_from_handle(handle)
             yield handle, object_to_data(person)
 
+    # Get numbers:
+
     def get_number_of_people(self):
         return len(self.proxy_map["Person"])
 
     def get_number_of_families(self):
         return len(self.proxy_map["Family"])
+
+    # Get from handles:
 
     @functools.cache
     def get_person_from_handle(self, handle):
@@ -130,6 +221,17 @@ class ProxyDbBase:
             raise HandleError(f"Handle {handle} not found")
 
     @functools.cache
+    def get_family_from_handle(self, handle):
+        if handle in self.proxy_map["Family"]:
+            family = self.db.get_family_from_handle(handle)
+            processed = self.proxy_process_family(family)
+            return processed
+        else:
+            raise HandleError(f"Handle {handle} not found")
+
+    # Get raw data:
+
+    @functools.cache
     def get_raw_person_data(self, handle):
         if handle in self.proxy_map["Person"]:
             person = self.db.get_person_from_handle(handle)
@@ -137,6 +239,8 @@ class ProxyDbBase:
             return object_to_data(processed)
         else:
             raise HandleError(f"Handle {handle} not found")
+
+    # Get from gramps id:
 
     @functools.cache
     def get_person_from_gramps_id(self, gramps_id):
@@ -151,6 +255,8 @@ class ProxyDbBase:
         else:
             return None
 
+    # Get all handles:
+
     def get_person_handles(self, sort_handles=False, locale=glocale):
         if sort_handles:
             sorted_handles = self.locale_sort("Person", locale.collation)
@@ -160,18 +266,13 @@ class ProxyDbBase:
             for handle in self.proxy_map["Person"]:
                 yield handle
 
-    @functools.cache
-    def get_family_from_handle(self, handle):
-        if handle in self.proxy_map["Family"]:
-            family = self.db.get_family_from_handle(handle)
-            processed = self.proxy_process_family(family)
-            return processed
-        else:
-            raise HandleError(f"Handle {handle} not found") from None
+    # Iter objects:
 
     def iter_people(self):
         for handle in self.proxy_map["Person"]:
             yield self.get_person_from_handle(handle)
+
+    # Iter handles:
 
     def iter_person_handles(self):
         for handle in self.proxy_map["Person"]:
@@ -180,6 +281,8 @@ class ProxyDbBase:
     def iter_family_handles(self):
         for handle in self.proxy_map["Family"]:
             yield handle
+
+    # Misc:
 
     def find_backlink_handles(self, handle, include_classes=None):
         for class_name, obj_handle in self.db.find_backlink_handles(
