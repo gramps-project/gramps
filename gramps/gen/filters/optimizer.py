d98fafccd3dc03805dc85e5d17e695b5ed80595d
Doug Blank <doug.blank@gmail.com>
2025-05-26 10:26:56 -0400

    More liniting

--- gramps/gen/filters/optimizer.py
+++ gramps/gen/filters/optimizer.py
@@ -56,7 +56,9 @@ def union(sets: List[Set[PrimaryObjectHandle]]) -> Set[PrimaryObjectHandle]:
 class Optimizer:
     def compute_potential_handles_for_filter(
         self, filter: GenericFilter
-    ) -> Tuple[Union[Set[PrimaryObjectHandle], None], Union[Set[PrimaryObjectHandle], None]]:
+    ) -> Tuple[
+        Union[Set[PrimaryObjectHandle], None], Union[Set[PrimaryObjectHandle], None]
+    ]:
         if len(filter.flist) == 0:
             return (None, None)
 
@@ -95,7 +97,9 @@ class Optimizer:
     def compute_potential_handles_for_rule(
         self,
         rule: Rule,
-    ) -> Tuple[Union[Set[PrimaryObjectHandle], None], Union[Set[PrimaryObjectHandle], None]]:
+    ) -> Tuple[
+        Union[Set[PrimaryObjectHandle], None], Union[Set[PrimaryObjectHandle], None]
+    ]:
         """ """
         if hasattr(rule, "selected_handles"):
             return (rule.selected_handles, None)
