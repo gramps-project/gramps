4325eaa38e2958863eea8ceb90e6366455346c6d
Steve Youngs <steve@youngs.cc>
2025-06-01 17:13:56 +0100

    Support optimization of filters using logical operations "one" and "or"
    If there is more than one rule, for "one" or "or", then all rules must be optimized in order to optimize the filter.

--- gramps/gen/filters/optimizer.py
+++ gramps/gen/filters/optimizer.py
@@ -31,7 +31,8 @@ import logging
 # Typing modules
 #
 # -------------------------------------------------------------------------
-from typing import List, Set, TYPE_CHECKING, Tuple
+from typing import Any, Callable, List, Set, TYPE_CHECKING, Tuple
+from typing_extensions import assert_never
 from .rules import Rule
 
 if TYPE_CHECKING:
@@ -45,24 +46,52 @@ class Optimizer:
     def compute_potential_handles_for_filter(
         self, filter: GenericFilter
     ) -> Tuple[Set[PrimaryObjectHandle] | None, Set[PrimaryObjectHandle] | None]:
-        if len(filter.flist) == 0:
-            return (None, None)
-
         handles_in = None
         handles_out = None
+
+        handles_in_set_op: Callable[
+            [Set[PrimaryObjectHandle], *Tuple[Set[PrimaryObjectHandle], ...]],
+            Set[PrimaryObjectHandle],
+        ]
+        handles_out_set_op: Callable[
+            [Set[PrimaryObjectHandle], *Tuple[Set[PrimaryObjectHandle], ...]],
+            Set[PrimaryObjectHandle],
+        ]
+        if filter.logical_op == "and" or len(filter.flist) == 1:
+            handles_in_set_op = set.intersection
+            handles_out_set_op = set.union
+            support_unoptimized_rule = True
+        elif filter.logical_op == "or":
+            handles_in_set_op = set.union
+            handles_out_set_op = set.intersection
+            support_unoptimized_rule = False  # with a logical_op of "or" it is not possible to optimize this filter if any rule is unoptimized
+        elif filter.logical_op == "one":
+            handles_in_set_op = set.difference
+            handles_out_set_op = set.intersection
+            support_unoptimized_rule = False  # with a logical_op of "one" it is not possible to optimize this filter if any rule is unoptimized
+        else:
+            assert_never(filter.logical_op)
+
         for rule in filter.flist:
-            if filter.logical_op == "and" or len(filter.flist) == 1:
-                rule_in, rule_out = self.compute_potential_handles_for_rule(rule)
-                if rule_in is not None:
-                    if handles_in is None:
-                        handles_in = rule_in
-                    else:
-                        handles_in = handles_in.intersection(rule_in)
-                if rule_out is not None:
-                    if handles_out is None:
-                        handles_out = rule_out
-                    else:
-                        handles_out = handles_out.union(rule_out)
+            rule_in, rule_out = self.compute_potential_handles_for_rule(rule)
+
+            if rule_in is not None:
+                if handles_in is None:
+                    handles_in = rule_in
+                else:
+                    handles_in = handles_in_set_op(handles_in, rule_in)
+            elif not support_unoptimized_rule:
+                # this rule is not optimized
+                # so break and return (None, None)
+                handles_in = None
+                handles_out = None
+                break
+
+            if rule_out is not None:
+                if handles_out is None:
+                    handles_out = rule_out
+                else:
+                    handles_out = handles_out_set_op(handles_out, rule_out)
 
         if filter.invert:
             handles_in, handles_out = handles_out, handles_in
@@ -70,14 +99,14 @@ class Optimizer:
         return handles_in, handles_out
 
     def compute_potential_handles_for_rule(
-        self,
-        rule: Rule,
+        self, rule: Rule
     ) -> Tuple[Set[PrimaryObjectHandle] | None, Set[PrimaryObjectHandle] | None]:
         """ """
         if hasattr(rule, "selected_handles"):
-            return (rule.selected_handles, None)
+            return (rule.selected_handles, set[PrimaryObjectHandle]())
         if hasattr(rule, "find_filter"):
             filter = rule.find_filter()
             if filter:
                 return self.compute_potential_handles_for_filter(filter)
+        # rule is not optimized so return (None, None)
         return (None, None)
