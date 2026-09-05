ec21deb1fbdfd3cb132bf08125955a8e65dbd91b
Nick Hall <nick-h@gramps-project.org>
2026-09-05 18:23:20 +0100

    Fix date modifiers for zh_TW

--- gramps/gen/datehandler/_date_zh_TW.py
+++ gramps/gen/datehandler/_date_zh_TW.py
@@ -58,8 +58,8 @@ class DateParserZH_TW(DateParser):
         "以前": Date.MOD_BEFORE,
         "以後": Date.MOD_AFTER,
         "大約": Date.MOD_ABOUT,
-        "from": Date.MOD_FROM,
-        "to": Date.MOD_TO,
+        "自": Date.MOD_FROM,
+        "至": Date.MOD_TO,
     }
 
     month_to_int = DateParser.month_to_int
